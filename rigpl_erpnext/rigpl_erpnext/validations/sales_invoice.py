1f62460ba92f6c51f87997d04e4c20c11109af59
Aditya Duggal <adityaduggal@gmail.com>
2015-04-25 16:19:30 +0530

    Minor fix to Sales Invoice validation for Non-Trial Invoices

--- rigpl_erpnext/rigpl_erpnext/validations/sales_invoice.py
+++ rigpl_erpnext/rigpl_erpnext/validations/sales_invoice.py
@@ -11,7 +11,6 @@ def validate(doc,method):
 	if (doc.letter_head != letter_head):
 		frappe.msgprint("Letter Head selected does not match with Sales Tax", raise_exception=1)
 	for d in doc.entries:
-		#frappe.msgprint(("Row#{0}: Item Code = {1} and SO# = {2}").format(d.idx, d.item_code, d.sales_order))
 		if d.sales_order is not None:
 			if d.delivery_note is None:
 				frappe.msgprint(("""Error in Row# {0} has SO# {1} but there is no DN.
@@ -21,8 +20,10 @@ def validate(doc,method):
 				frappe.msgprint(("""Error in Row# {0} has DN# {1} but there is no SO.
 				Hence making of Invoice is DENIED""").format(d.idx, d.delivery_note), raise_exception=1)
 		dnd = frappe.get_doc("Delivery Note Item", d.dn_detail)
-		sod = dnd.prevdoc_detail_docname
-		tt = frappe.get_doc("Trial Tracking", dnd.prevdoc_detail_docname)
-		if tt:
-			if tt.status is not ("Failed", "Passed") or tt.docstatus <> 1:
-				frappe.msgprint("Cannot Make Invoice for Trial Items without Trial being Completed and Submitted", raise_exception=1)
+		so = frappe.get_doc("Sales Order", d.sales_order)
+		if so.track_trial == 1:
+			sod = dnd.prevdoc_detail_docname
+			tt = frappe.get_doc("Trial Tracking", dnd.prevdoc_detail_docname)
+			if tt:
+				if tt.status is not ("Failed", "Passed") or tt.docstatus <> 1:
+					frappe.msgprint("Cannot Make Invoice for Trial Items without Trial being Completed and Submitted", raise_exception=1)
