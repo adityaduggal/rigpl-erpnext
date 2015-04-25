b04560540e446d533e66d459b35d3b0853d17967
Aditya Duggal <adityaduggal@gmail.com>
2015-04-25 16:40:44 +0530

    Minor Fix for Trial Tracking Validations in DN and SI

--- rigpl_erpnext/rigpl_erpnext/validations/delivery_note.py
+++ rigpl_erpnext/rigpl_erpnext/validations/delivery_note.py
@@ -5,6 +5,8 @@ from frappe import msgprint
 
 def on_submit(doc, method):
 	for dnd in doc.get("delivery_note_details"):
-		tt = frappe.get_doc("Trial Tracking", dnd.prevdoc_detail_docname)
-		if tt:
-			frappe.msgprint('{0}{1}'.format("Please update Trial Tracking No: ", dnd.prevdoc_detail_docname))
+		so = frappe.get_doc("Sales Order", dnd.against_sales_order)
+		if so.track_trial == 1:
+			tt = frappe.get_doc("Trial Tracking", dnd.prevdoc_detail_docname)
+			if tt:
+				frappe.msgprint('{0}{1}'.format("Please update Trial Tracking No: ", dnd.prevdoc_detail_docname))
--- rigpl_erpnext/rigpl_erpnext/validations/sales_invoice.py
+++ rigpl_erpnext/rigpl_erpnext/validations/sales_invoice.py
@@ -19,11 +19,12 @@ def validate(doc,method):
 			if d.sales_order is None:
 				frappe.msgprint(("""Error in Row# {0} has DN# {1} but there is no SO.
 				Hence making of Invoice is DENIED""").format(d.idx, d.delivery_note), raise_exception=1)
-		dnd = frappe.get_doc("Delivery Note Item", d.dn_detail)
-		so = frappe.get_doc("Sales Order", d.sales_order)
-		if so.track_trial == 1:
-			sod = dnd.prevdoc_detail_docname
-			tt = frappe.get_doc("Trial Tracking", dnd.prevdoc_detail_docname)
-			if tt:
-				if tt.status is not ("Failed", "Passed") or tt.docstatus <> 1:
-					frappe.msgprint("Cannot Make Invoice for Trial Items without Trial being Completed and Submitted", raise_exception=1)
+		frappe.msgprint('{0}{1}'.format("yo ",d.sales_order))
+		if d.sales_order is not None:
+			if so.track_trial == 1:
+				dnd = frappe.get_doc("Delivery Note Item", d.dn_detail)
+				sod = dnd.prevdoc_detail_docname
+				tt = frappe.get_doc("Trial Tracking", dnd.prevdoc_detail_docname)
+				if tt:
+					if tt.status is not ("Failed", "Passed") or tt.docstatus <> 1:
+						frappe.msgprint("Cannot Make Invoice for Trial Items without Trial being Completed and Submitted", raise_exception=1)
