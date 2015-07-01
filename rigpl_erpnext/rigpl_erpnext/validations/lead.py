cce34202cf9bd7c07a64c2f2207f659c1e6fe0df
Aditya Duggal <adityaduggal@gmail.com>
2015-07-01 19:04:36 +0530

    Minor fix to lead validations

--- rigpl_erpnext/rigpl_erpnext/validations/lead.py
+++ rigpl_erpnext/rigpl_erpnext/validations/lead.py
@@ -6,8 +6,9 @@ import frappe.permissions
 
 def on_update(doc,method):
 	if doc.lead_owner:
-		frappe.db.set_value("Lead", doc.name, "contact_by", doc.lead_owner)
 		frappe.permissions.add_user_permission("Lead", doc.name, doc.lead_owner)
+		if doc.lead_owner <> doc.contact_by:
+			frappe.db.set_value("Lead", doc.name, "contact_by", doc.lead_owner)
 	
 	#Check if the lead is not in another user, if its there then delete the LEAD from the user's permission
 	query = """SELECT name, parent from `tabDefaultValue` where defkey = 'Lead' AND defvalue = '%s' AND parent <> '%s' """ % (doc.name, doc.lead_owner)
