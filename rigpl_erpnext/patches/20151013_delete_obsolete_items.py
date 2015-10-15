d98013b993d294d656d6a1ffd1e1a2f26b9f32b1
Aditya Duggal <adityaduggal@gmail.com>
2015-10-15 14:07:28 +0530

    Patch to delete all Non-Variant Old Items without transactions

--- rigpl_erpnext/patches/20151013_delete_obsolete_items.py
+++ rigpl_erpnext/patches/20151013_delete_obsolete_items.py
@@ -8,11 +8,8 @@ def execute():
 		SELECT it.name, ifnull(it.owner, "Administrator"), it.creation
 		FROM `tabItem` it
 		WHERE 
-			it.pl_item = 'No'
-			AND (it.tool_type = 'Round' OR it.tool_type = 'Punches' 
-			OR it.tool_type = 'Square' OR it.tool_type = 'Rectangular' 
-			OR it.tool_type = 'Punch Step3' OR it.tool_type = 'Mandrels'
-			OR it.tool_type = 'Others' OR it.tool_type = 'Parting')
+			it.variant_of IS NULL
+			AND it.has_variants <> 1
 
 			AND ifnull((SELECT count(sod.name) FROM `tabMaterial Request Item` sod WHERE sod.item_code = 
 				it.name GROUP BY sod.item_code),0) = 0
