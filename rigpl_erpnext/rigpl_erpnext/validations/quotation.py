efee098b0bcbe62fba6023e38fb2dbec7e167426
Aditya Duggal <adityaduggal@gmail.com>
2016-01-18 11:26:31 +0530

    Validation for Lead in quote and opportunity added

--- rigpl_erpnext/rigpl_erpnext/validations/quotation.py
+++ rigpl_erpnext/rigpl_erpnext/validations/quotation.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import msgprint
 
-def on_update(doc,method):
+def validate(doc,method):
 	#Check if Lead is Converted or Not, if the lead is converted 
 	#then don't allow it to be selected without linked customer
 	if doc.lead:
@@ -13,7 +13,7 @@ def on_update(doc,method):
 			frappe.throw(("Lead {0} is Linked to Customer {1} so kindly make quotation for \
 				Customer and not Lead").format(doc.lead, link[0][0]))
 		elif doc.customer:
-			if doc.customer != link[0][0]
+			if doc.customer != link[0][0]:
 				frappe.throw(("Customer {0} is not linked to Lead {1} hence cannot be set\
 				in the Quotation").format(doc.customer, doc.lead))
 		
