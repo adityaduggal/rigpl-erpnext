5a8ce87a9e2b6681c676bc89be0796cb47465dae
Aditya Duggal <adityaduggal@gmail.com>
2016-04-09 15:16:20 +0530

    Minor fix on doctype

---
+++ rigpl_erpnext/rigpl_erpnext/doctype/roster/roster.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Rohit Industries Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Roster', {
+	refresh: function(frm) {
+
+	}
+});
