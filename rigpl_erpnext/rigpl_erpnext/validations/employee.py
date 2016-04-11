83b714652286b84882711dde424f971f2763c289
Aditya Duggal <adityaduggal@gmail.com>
2016-04-11 18:20:56 +0530

    Minor Fix

--- rigpl_erpnext/rigpl_erpnext/validations/employee.py
+++ rigpl_erpnext/rigpl_erpnext/validations/employee.py
@@ -14,9 +14,8 @@ def validate(doc,method):
 	if relativedelta(doj, dob).years < 18:
 		frappe.msgprint("Not Allowed to Create Employees under 18 years of Age", raise_exception = 1)
 	if doc.relieving_date:
-		if doc.relieving_date < time.strftime("%Y-%m-%d"):
-			if doc.status <> "Left":
-				frappe.msgprint("Status has to be 'LEFT' as the Relieving Date is populated",raise_exception =1)
+		if doc.status <> "Left":
+			frappe.msgprint("Status has to be 'LEFT' as the Relieving Date is populated",raise_exception =1)
 	#Generate employee number on the following logic
 	#Employee Number would be YYYYMMDDXXXXC, where:
 	#YYYYMMDD = Date of Joining in YYYYMMDD format
