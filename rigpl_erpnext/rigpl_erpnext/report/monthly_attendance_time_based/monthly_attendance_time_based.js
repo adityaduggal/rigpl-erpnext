d6c75873d21146f749c5fe594196f0ea5c824751
Aditya Duggal <adityaduggal@gmail.com>
2016-04-13 15:19:03 +0530

    Added Roster and Leave Application Report

--- rigpl_erpnext/rigpl_erpnext/report/monthly_attendance_time_based/monthly_attendance_time_based.js
+++ rigpl_erpnext/rigpl_erpnext/report/monthly_attendance_time_based/monthly_attendance_time_based.js
@@ -10,19 +10,49 @@ frappe.query_reports["Monthly Attendance Time Based"] = {
 			"options": "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
 			"default": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", 
 				"Dec"][frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth()],
+			"reqd": 1
 		},
 		{
-			"fieldname":"fiscal_year",
-			"label": __("Fiscal Year"),
-			"fieldtype": "Link",
-			"options": "Fiscal Year",
-			"default": sys_defaults.fiscal_year,
+			"fieldname":"year",
+			"label": __("Year"),
+			"fieldtype": "Select",
+			"reqd": 1
 		},
 		{
 			"fieldname":"employee",
 			"label": __("Employee"),
 			"fieldtype": "Link",
 			"options": "Employee"
+		},
+		{
+			"fieldname":"branch",
+			"label": __("Branch"),
+			"fieldtype": "Link",
+			"options": "Branch"
+		},
+		{
+			"fieldname":"department",
+			"label": __("Department"),
+			"fieldtype": "Link",
+			"options": "Department"
+		},
+		{
+			"fieldname":"designation",
+			"label": __("Designation"),
+			"fieldtype": "Link",
+			"options": "Designation"
 		}
-	]
+	],
+	"onload": function(me) {
+		return  frappe.call({
+			method: "erpnext.hr.report.monthly_attendance_sheet.monthly_attendance_sheet.get_attendance_years",
+			callback: function(r) {
+				var year_filter = me.filters_by_name.year;
+				year_filter.df.options = r.message;
+				year_filter.df.default = r.message.split("\n")[0];
+				year_filter.refresh();
+				year_filter.set_input(year_filter.df.default);
+			}
+		});
+	}
 }
--- rigpl_erpnext/rigpl_erpnext/report/monthly_attendance_time_based/monthly_attendance_time_based.py
+++ rigpl_erpnext/rigpl_erpnext/report/monthly_attendance_time_based/monthly_attendance_time_based.py
@@ -1,11 +1,11 @@
-# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: GNU General Public License v3. See license.txt
 
 from __future__ import unicode_literals
 import frappe
-import math
-from frappe.utils import cstr, cint
+from frappe.utils import cstr, cint, getdate
 from frappe import msgprint, _
+from calendar import monthrange
 
 def execute(filters=None):
 	if not filters: filters = {}
@@ -13,37 +13,22 @@ def execute(filters=None):
 	conditions, filters = get_conditions(filters)
 	columns = get_columns(filters)
 	att_map = get_attendance_list(conditions, filters)[0]
-	attendance_list = get_attendance_list(conditions, filters)[1]
-	time_map = get_attendance_list(conditions, filters)[2]
+	ot_map = get_attendance_list(conditions,filters)[1]
 	emp_map = get_employee_details()
-	holidays = get_holiday_list(conditions, filters)
-	total_days = filters["total_days_in_month"]
+
 	data = []
-	no_of_employees = len(sorted(att_map))
-	
-	#frappe.msgprint(time_map)
-	
-	for emp in sorted(att_map):
-		nos = sum(1 for i in attendance_list if i['day_of_month']==1)
-	
-	for emp in sorted(att_map):
-		emp_det = emp_map.get(emp) #emp_det == Employee Details from Employee master
+	for emp in sorted(ot_map):
+		emp_det = emp_map.get(emp)
 		if not emp_det:
 			continue
 
-		row = [emp, emp_det.employee_name, emp_det.employee_number]
-		row2 = [emp, emp_det.employee_name, emp_det.employee_number]
-		
-		total_p = total_a = total_h = 0.0
-		for day in range(total_days):
-			stat = ""
-			for i in range(len(holidays)):
-				if day+1 == holidays[i][0]:
-					stat = "Holiday"
-			if stat != "Holiday": stat = "Absent"
-			date_time = att_map.get(emp).get(day + 1, stat)
-			status = att_map.get(emp).get(day + 1, stat)
-			status_map = {"Present": "P", "Absent": "A", "Half Day": "HD", "Holiday": "HO"}
+		row = [emp, emp_det.employee_name, emp_det.branch, emp_det.department, emp_det.designation,
+			emp_det.company]
+
+		total_p = total_a = 0.0
+		for day in range(filters["total_days_in_month"]):
+			status = ot_map.get(emp).get(day + 1, "None")
+			status_map = {"Present": "P", "Absent": "A", "Half Day": "H", "None": ""}
 			row.append(status_map[status])
 
 			if status == "Present":
@@ -53,85 +38,61 @@ def execute(filters=None):
 			elif status == "Half Day":
 				total_p += 0.5
 				total_a += 0.5
-			elif status == "Holiday":
-				total_h += 1
-			
-			total_s = total_p + math.ceil(total_h/total_days*total_p)
 
-		row += [total_p, total_a, total_s]
-
+		row += [total_p, total_a]
 		data.append(row)
-		data.append(row2)
 
 	return columns, data
 
 def get_columns(filters):
 	columns = [
-		_("Emp ID") + ":Link/Employee:80", _("Emp Name") + "::140", 
-		_("Emp No") + "::100"
+		_("Employee") + ":Link/Employee:120", _("Employee Name") + "::140", _("Branch")+ ":Link/Branch:120",
+		_("Department") + ":Link/Department:120", _("Designation") + ":Link/Designation:120",
+		 _("Company") + ":Link/Company:120"
 	]
 
 	for day in range(filters["total_days_in_month"]):
 		columns.append(cstr(day+1) +"::20")
 
-	columns += [_("Total Present") + ":Float:80", _("Total Absent") + ":Float:80", _("Total Salaried Days") + ":Float:80"]
+	columns += [_("Total Present") + ":Float:80", _("Total Absent") + ":Float:80"]
 	return columns
 
 def get_attendance_list(conditions, filters):
-	attendance_list = frappe.db.sql("""select at.employee, day(at.att_date) as day_of_month,
-		at.status, att.time_type, att.date_time from tabAttendance at, `tabAttendance Time Table` att
-		where at.docstatus = 1 AND att.parent = at.name
-		%s order by at.employee, at.att_date, att.date_time""" %
+	attendance_list = frappe.db.sql("""select employee, day(att_date) as day_of_month,
+		status, overtime from tabAttendance where docstatus = 1 %s order by employee, att_date""" %
 		conditions, filters, as_dict=1)
-	
-	#frappe.msgprint(attendance_list)
+
 	att_map = {}
-	time_map ={}
+	ot_map = {}
 	for d in attendance_list:
 		att_map.setdefault(d.employee, frappe._dict()).setdefault(d.day_of_month, "")
 		att_map[d.employee][d.day_of_month] = d.status
-		time_map.setdefault(d.employee, frappe._dict()).setdefault(d.day_of_month, "")
-		frappe.msgprint(time_map)
-		time_map[d.employee][d.day_of_month] = d.date_time
-	
-	#frappe.msgprint(time_map)
-	return att_map, attendance_list, time_map
+		ot_map.setdefault(d.employee, frappe._dict()).setdefault(d.day_of_month, "")
+		ot_map[d.employee][d.day_of_month] = d.overtime
+	frappe.msgprint(ot_map)
 
+	return att_map, ot_map
+	
 def get_conditions(filters):
-	if not (filters.get("month") and filters.get("fiscal_year")):
+	if not (filters.get("month") and filters.get("year")):
 		msgprint(_("Please select month and year"), raise_exception=1)
 
 	filters["month"] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
-		"Dec"].index(filters["month"]) + 1
+		"Dec"].index(filters.month) + 1
 
-	from calendar import monthrange
-	filters["total_days_in_month"] = monthrange(cint(filters["fiscal_year"].split("-")[-1]),
-		filters["month"])[1]
+	filters["total_days_in_month"] = monthrange(cint(filters.year), filters.month)[1]
 
-	conditions = " and month(att_date) = %(month)s and fiscal_year = %(fiscal_year)s"
-
-	if filters.get("company"): conditions += " and company = %(company)s"
+	conditions = " and month(att_date) = %(month)s and year(att_date) = %(year)s"
+	
 	if filters.get("employee"): conditions += " and employee = %(employee)s"
 
 	return conditions, filters
 
 def get_employee_details():
 	emp_map = frappe._dict()
-	for d in frappe.db.sql("""select name, employee_name, employee_number,
-		designation,department, branch, company
-		from tabEmployee where docstatus < 2
-		and status = 'Active'""", as_dict=1):
+	for d in frappe.db.sql("""select name, employee_name, designation,
+		department, branch, company
+		from tabEmployee""", as_dict=1):
 		emp_map.setdefault(d.name, d)
 
 	return emp_map
-
-def get_holiday_list(conditions, filters):
-	fy = filters.get("fiscal_year")
-	month = (filters["month"])
-	query = """select day(hol.holiday_date) as holiday from `tabHoliday List` hl, `tabHoliday` hol
-		WHERE hl.fiscal_year = "%s" AND hol.parent = hl.name 
-		AND MONTH(hol.holiday_date) = %s 
-		ORDER BY hol.holiday_date ASC""" %(fy, month)
-
-	hol = frappe.db.sql(query, as_list=1)
-	return hol
