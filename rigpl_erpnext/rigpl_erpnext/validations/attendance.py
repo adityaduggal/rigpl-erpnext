b434e67cb7a94c4988352ce635d7fe2cdd3da4ec
Aditya Duggal <adityaduggal@gmail.com>
2016-05-20 19:26:21 +0530

    Minor Fix

--- rigpl_erpnext/rigpl_erpnext/validations/attendance.py
+++ rigpl_erpnext/rigpl_erpnext/validations/attendance.py
@@ -15,8 +15,8 @@ def validate(doc,method):
 	att_tt = []
 	att_time = []
 	att_date = getdate(doc.att_date)
-	if doc.status <> "Present" or doc.status <> "Half Day":
-		frappe.throw(("Only Present Attendance is Allowed Check {0}").format(doc.name))
+	if doc.status <> "Present" and doc.status <> "Half Day":
+		frappe.throw(("Only Present or Half Day Attendance is Allowed Check {0}").format(doc.name))
 
 	check_employee (doc, method)
 	shft = get_shift(doc,method)
