68980f522e0a2f9253c4c0846e0926c29a7c141f
Aditya Duggal <adityaduggal@gmail.com>
2017-07-06 18:20:25 +0530

    Minor Fixes to Roster

--- rigpl_erpnext/rigpl_erpnext/doctype/roster/roster.js
+++ rigpl_erpnext/rigpl_erpnext/doctype/roster/roster.js
@@ -10,6 +10,6 @@ frappe.ui.form.on('Roster', {
 frappe.ui.form.on("Roster Details", "employee", function(frm, cdt, cdn){
 	var d = frappe.get_doc(cdt, cdn);
 	frappe.model.set_value(cdt, cdn, "from_date", frm.doc.from_date);
-	frappe.model.set_value(cdt, cdn, "to_date", frm.doc.to_date)
+	frappe.model.set_value(cdt, cdn, "to_date", frm.doc.to_date);
 	cur_frm.refresh_fields();
 });
