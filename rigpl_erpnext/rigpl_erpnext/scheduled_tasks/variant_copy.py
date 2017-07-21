c8b440093f3118841b91b0d5165409aaffefd23b
Aditya Duggal <adityaduggal@gmail.com>
2017-07-21 17:00:02 +0530

    Minor Fix -Patch to run manually to change the fieldname based on attributes -Also minor fix on the variant copy logic

--- rigpl_erpnext/rigpl_erpnext/scheduled_tasks/variant_copy.py
+++ rigpl_erpnext/rigpl_erpnext/scheduled_tasks/variant_copy.py
@@ -11,7 +11,7 @@ import json
 #Run this script every hour but to ensure that there is no server overload run it only for 1 template at a time
 
 def check_wrong_variants():
-	limit = frappe.db.get_single_value("Stock Settings", "automatic_sync_field_limit")
+	limit_set = int(frappe.db.get_single_value("Stock Settings", "automatic_sync_field_limit"))
 	is_sync_allowed = frappe.db.get_single_value("Stock Settings", 
 		"automatically_sync_templates_data_to_items")
 	if is_sync_allowed == 1:
@@ -24,23 +24,27 @@ def check_wrong_variants():
 		fields_edited = 0
 		for t in templates:
 			print (str(t[0]) + " Has No of Variants = " + str(t[1]))
-			if fields_edited <= limit:
+			if fields_edited <= limit_set:
 				temp_doc = frappe.get_doc("Item", t[0])
 				variants = frappe.db.sql("""SELECT name FROM `tabItem` WHERE variant_of = '%s'
 					ORDER BY modified DESC"""%(t[0]), as_list=1)
 				#Check all variants' fields are matching with template if 
 				#not then copy the fields else go to next item
 				for item in variants:
+					print ("Checking Item = " + item[0])
 					it_doc = frappe.get_doc("Item", item[0])
 					fields_edited += check_and_copy_attributes_to_variant(temp_doc, it_doc)
+					#print (str(fields_edited) + " ##################")
 			else:
-				print ("Limit of " + str(limit) + " fields reached. Run again for more updating")
-				exit()
+				print ("Limit of " + str(limit_set) + " fields reached. Run again for more updating")
+				break
+				
 
 
 def check_and_copy_attributes_to_variant(template, variant):
 	from frappe.model import no_value_fields
 	check = 0
+	save_chk = 0
 	exclude_fields = ["item_code", "item_name", "show_in_website", "variant_of", "has_variants", 
 		"description", "web_long_description", "item_variant_restrictions"]
 	for field in template.meta.fields:
@@ -49,8 +53,11 @@ def check_and_copy_attributes_to_variant(template, variant):
 			and (not field.no_copy) and field.fieldname not in exclude_fields:
 			if variant.get(field.fieldname) != template.get(field.fieldname):
 				variant.set(field.fieldname, template.get(field.fieldname))
+				save_chk = 1
 				print ("Updated Item " + variant.name + " Field Changed = " + str(field.label) + 
 					" Updated Value to " + str(template.get(field.fieldname)))
-				variant.save()
 				check += 1
+	if save_chk == 1:
+		variant.save()
+		print ("Item Code " + variant.name + " Saved")
 	return check
