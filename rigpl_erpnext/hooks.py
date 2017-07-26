e74e43a34bad381fdae45f9086bfffd266920b95
Aditya Duggal <adityaduggal@gmail.com>
2017-07-26 13:23:18 +0530

    Added hourly hooks for the shipment data

--- rigpl_erpnext/hooks.py
+++ rigpl_erpnext/hooks.py
@@ -171,7 +171,9 @@ scheduler_events = {
 # 		"rigpl_erpnext.rigpl_erpnext.scheduled_tasks.communication.daily"
 # 	],
  	"hourly": [
- 		"rigpl_erpnext.rigpl_erpnext.scheduled_tasks.communication.daily"
+ 		"rigpl_erpnext.rigpl_erpnext.scheduled_tasks.communication.daily",
+ 		"rigpl_erpnext.rigpl_erpnext.scheduled_tasks.shipment_data_update.send_bulk_tracks",
+ 		"rigpl_erpnext.rigpl_erpnext.scheduled_tasks.shipment_data_update.get_all_ship_data"
  	],
  	"daily": [
  		"rigpl_erpnext.rigpl_erpnext.scheduled_tasks.variant_copy.check_wrong_variants"
