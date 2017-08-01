d3c4b6894d577790056c645320a8c4f3fa5c9013
Aditya Duggal <adityaduggal@gmail.com>
2017-08-01 11:30:09 +0530

    Sales Invoice Changes -Now Update stock is mandatory only for Stock Items without DN

--- rigpl_erpnext/rigpl_erpnext/validations/sales_invoice.py
+++ rigpl_erpnext/rigpl_erpnext/validations/sales_invoice.py
@@ -23,60 +23,50 @@ def check_delivery_note_rule(doc,method):
 		without SO and then make INVOICE is not POSSIBLE, means you SO => DN => SI or only SI also
 		denies SO => SI
 	'''
-	list_of_dns = []
-	list_of_dn_details = []
+	dn_dict = frappe._dict()
+	list_of_dn_dict = []
 	
 	for d in doc.items:
-		if d.delivery_note not in list_of_dns:
-			list_of_dns.extend([d.delivery_note])
-		
-		if d.dn_detail not in list_of_dn_details:
-			list_of_dn_details.extend([d.dn_detail])
-			
-		if d.sales_order is not None:
-			if d.delivery_note is None:
-				#Rule no.5 in the above description for disallow SO=>SI no skipping DN
-				frappe.msgprint(("""Error in Row# {0} has SO# {1} but there is no DN.
-				Hence making of Invoice is DENIED""").format(d.idx, d.sales_order), \
-				raise_exception=1)
-		elif d.delivery_note is not None:
-			if d.sales_order is None:
-				frappe.msgprint(("""Error in Row# {0} has DN# {1} but there is no SO.
-				Hence making of Invoice is DENIED""").format(d.idx, d.delivery_note), \
-				raise_exception=1)
-			
-			#Check the quantity of Invoice is EQUAL to the DN quantity and also check if 
-			#the FULL DN is being invoices
-			#Below code would check if the items are based on DN or NOT
-			#If SI is NOT BASED on DN then it would ensure that the SI has the 
-			#update stock button checked.
-			
-			dn = frappe.get_doc("Delivery Note", d.delivery_note)
-			if dn is not None:
-				for dnd in dn.items:
-					if dnd.name == d.dn_detail:
-						if d.qty > 0:
-							if dnd.qty != d.qty:
-								frappe.msgprint(("""Invoice Qty should be equal to DN Qty \
-									in line # {0}""").format(d.idx), raise_exception=1)
+		#Stock Items without DN would need Update Stock Check
+		if d.delivery_note is None:
+			item_doc = frappe.get_doc('Item', d.item_code)
+			if item_doc.is_stock_item == 1 and doc.update_stock != 1:
+				frappe.throw(("Item Code {0} in Row # {1} is Stock Item \
+					without any DN so please check Update Stock Button\
+					").format(d.item_code, d.idx))
 
-	if len(list_of_dns)==1 and list_of_dns[0] == None:
-		if doc.update_stock != 1:
-			for d in doc.items:
-				if d.qty > 0:
-					frappe.msgprint("Please check the Update Stock Button since Items are \
-						not based on DN", raise_exception=1)
-	elif len(list_of_dns)>1:
-		for i in range(len(list_of_dns)):
-			if list_of_dns[i] is None:
-				frappe.msgprint(("""Not allowed to enter items without DN make another invoice \
-					for such items check line # {0}""").format((d.idx)+1), raise_exception=1)
-			else:
-				dn = frappe.get_doc("Delivery Note", list_of_dns[i])
-				for d in dn.items:
-					if d.name not in list_of_dn_details:
-						frappe.msgprint(("""Item Code {0} in DN# {1} is not mentioned in \
-						the Invoice""").format(d.item_code, dn.name), raise_exception=1)
+		if d.dn_detail not in list_of_dn_dict and d.delivery_note is not None:
+
+			dn_dict['dn'] = d.delivery_note
+			dn_dict['dn_detail'] = d.dn_detail
+			dn_dict['item_code'] = d.item_code
+			list_of_dn_dict.append(dn_dict.copy())
+		#With SO DN is mandatory
+		if d.sales_order is not None and d.delivery_note is None:
+			#Rule no.5 in the above description for disallow SO=>SI no skipping DN
+			frappe.msgprint(("""Error in Row# {0} has SO# {1} but there is no DN.
+			Hence making of Invoice is DENIED""").format(d.idx, d.sales_order), \
+			raise_exception=1)
+		#With DN SO is mandatory
+		if d.delivery_note is not None and d.sales_order is None:
+			frappe.msgprint(("""Error in Row# {0} has DN# {1} but there is no SO.
+			Hence making of Invoice is DENIED""").format(d.idx, d.delivery_note), \
+			raise_exception=1)
+		#For DN items quantities should be same
+		if d.delivery_note is not None:
+			dn_qty = frappe.db.get_value ('Delivery Note Item', d.dn_detail, 'qty')
+			if dn_qty != d.qty:
+				frappe.throw(("Invoice Qty should be equal to DN quantity of \
+					{0} at Row # {1}").format(dn_qty, d.idx))
+	if list_of_dn_dict:
+		unique_dn = {v['dn']:v for v in list_of_dn_dict}.values()
+		for dn in unique_dn:
+			dn_doc = frappe.get_doc('Delivery Note', dn.dn)
+			for d in dn_doc.items:
+				if not any (x['dn_detail'] == d.name for x in list_of_dn_dict):
+					frappe.throw(("Item No: {0} with Item Code: {1} in DN# {2} \
+						is not mentioned in SI# {3}").format(d.idx, d.item_code, \
+						dn_doc.name, doc.name))
 
 def check_gst_rules(doc,method):
 	series_template = frappe.db.get_value("Sales Taxes and Charges Template", \
