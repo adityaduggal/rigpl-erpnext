2ac050184ed4889fb4d93772e899914d9d29c66b
Aditya Duggal <adityaduggal@gmail.com>
2018-01-19 20:41:01 +0530

    Added Further custom code for Shipments wit Fedex

--- rigpl_erpnext/rigpl_erpnext/doctype/book_shipment/book_shipment.js
+++ rigpl_erpnext/rigpl_erpnext/doctype/book_shipment/book_shipment.js
@@ -6,3 +6,15 @@ frappe.ui.form.on('Book Shipment', {
 
 	}
 });
+
+frappe.ui.form.on("Shipment Package Details", "package_weight", function(frm, cdt, cdn) {
+// code for calculate total and set on parent field.
+	weight = 0;
+	uom = "";
+	$.each(frm.doc.shipment_package_details || [], function(i, d) {
+		weight += flt(d.package_weight);
+		uom = d.weight_uom
+	});
+	frm.set_value("total_weight", weight);
+	frm.set_value("weight_uom", uom);
+});
