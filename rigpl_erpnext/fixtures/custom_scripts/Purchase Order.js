6f051305c0ca5e6425048d8ee6362f5e9d96cb26
Aditya Duggal <adityaduggal@gmail.com>
2018-11-15 19:03:04 +0530

    Fixes to PO Custom Scripts

--- rigpl_erpnext/fixtures/custom_scripts/Purchase Order.js
+++ rigpl_erpnext/fixtures/custom_scripts/Purchase Order.js
@@ -1,45 +1,43 @@
 cur_frm.cscript.is_subcontracting = function(doc, cdt, cdn) {
-	cur_frm.set_query("item_code", "items", function(){
-		if (cur_frm.doc.is_subcontracting == 1){
-			return{
-				query: "erpnext.controllers.queries.item_query",
-				filters:{ 'is_job_work': 1 }
-			}
-		}
-		else {
-			return{
-				query: "erpnext.controllers.queries.item_query",
-				filters:{'is_purchase_item':1}
-			}
-		}
-	});
+    cur_frm.set_query("item_code", "items", function(){
+        if (cur_frm.doc.is_subcontracting == 1){
+            return{
+                query: "erpnext.controllers.queries.item_query",
+                filters:{ 'is_job_work': 1 }
+            }
+        }
+        else {
+            return{
+                query: "erpnext.controllers.queries.item_query",
+                filters:{'is_purchase_item':1}
+            }
+        }
+    });
 };
 frappe.ui.form.on("Purchase Order", "refresh", function(frm) {
 
-// Get Items from Production Order
-// Get Items from Production Order
-cur_frm.add_custom_button(__('Production Order'),
+// Get Items from Work Order
+cur_frm.add_custom_button(__('Work Order'),
     function() {
-    var me = this;
-    this.dialog = new frappe.ui.Dialog({
-        title: "Get Items from Production Order",
+        var dialog = new frappe.ui.Dialog({
+        title: "Get Items from Work Order",
         fields: [
-            {"fieldtype": "Link", "label": __("Production Order"), "fieldname": "production_order","options":'Production Order',"reqd": 1, 
+            {"fieldtype": "Link", "label": __("Work Order"), "fieldname": "production_order","options":'Work Order',"reqd": 1, 
                 get_query: function() { return {query: "rigpl_erpnext.rigpl_erpnext.validations.purchase_order.get_pending_prd"}},
             },
             {"fieldtype": "Button", "label": __("Get"), "fieldname": "get"},
             {"fieldtype": "HTML", "fieldname": "production_order_items_details"},
         ],
     });
-    me.dialog.show();
-    fd = this.dialog.fields_dict;
-    this.dialog.fields_dict.get.$input.click(function() {
-        value = me.dialog.get_values();
+    dialog.show();
+    var fd = dialog.fields_dict;
+    dialog.fields_dict.get.$input.click(function() {
+        var value = dialog.get_values();
 
     frappe.call({    
         method: "frappe.client.get_list",
            args: {
-            doctype: "Production Order",
+            doctype: "Work Order",
                fields: ["production_item", "item_description ", "sales_order", "so_detail", "qty","fg_warehouse","stock_uom","name"],
                filters: { "name" : value.production_order
                 },
@@ -48,12 +46,12 @@ cur_frm.add_custom_button(__('Production Order'),
             if(res && res.message){
                     var row = frappe.model.add_child(cur_frm.doc, "Purchase Order Item", "items");
                     row.qty = res.message[0]['qty'];
-					if (cur_frm.doc.is_subcontracting == 1){
-						row.subcontracted_item = res.message[0]['production_item'];
-					} else{
-						row.item_code = res.message[0]['production_item'];
-					}
-					row.description = res.message[0]['item_description'];
+                    if (cur_frm.doc.is_subcontracting == 1){
+                        row.subcontracted_item = res.message[0]['production_item'];
+                    } else{
+                        row.item_code = res.message[0]['production_item'];
+                    }
+                    row.description = res.message[0]['item_description'];
                     row.so_detail = res.message[0]['so_detail'];
                     row.uom = res.message[0]['stock_uom'];
                     row.stock_uom = res.message[0]['stock_uom'];
@@ -66,9 +64,9 @@ cur_frm.add_custom_button(__('Production Order'),
 
     });
 
-    add_production_order_items_to_stock = function(){
+    var add_production_order_items_to_stock = function(){
         var items_to_add = []
-        value = me.dialog.get_values();
+        var value = dialog.get_values();
         $.each($(fd.production_order_items_details.wrapper).find(".select:checked"), function(name, item){
             items_to_add.push($(item).val());
         });
@@ -76,12 +74,12 @@ cur_frm.add_custom_button(__('Production Order'),
             for(i=0;i<items_to_add.length;i++){
                 add_production_order_items(items_to_add,i)
             }
-            me.dialog.hide()
+            dialog.hide()
         }    
         else{
             msgprint("Select Item Before Add")
         }
     }
-}, __("Add items from"));
+}, __("Get items from"));
 
 });
