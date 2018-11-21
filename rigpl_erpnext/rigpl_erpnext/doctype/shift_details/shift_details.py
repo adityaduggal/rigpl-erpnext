000419efbe205e4cf0203f9daded9de6936ec673
Aditya Duggal <adityaduggal@gmail.com>
2018-11-21 19:00:44 +0530

    Deleted the following custom doctypes - Roster (Now roster is handled by Shift Request and Shift Assignment in ERPNext) - Salary Slip Payment (Old table being removed was not being used) - Shift Details (Now this table is handled by Shift Type in ERPNext)

--- rigpl_erpnext/rigpl_erpnext/doctype/shift_details/shift_details.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2013, Rohit Industries Ltd. and contributors
-# For license information, please see license.txt
-
-from __future__ import unicode_literals
-import frappe
-from frappe.model.document import Document
-
-class ShiftDetails(Document):
-	pass
