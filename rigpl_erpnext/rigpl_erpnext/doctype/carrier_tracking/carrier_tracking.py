d7c0a78bda1fe6eee4f5a445c8ac0eb2fc5b4033
Aditya Duggal <adityaduggal@gmail.com>
2019-02-18 16:55:30 +0530

    Minor Fix on PO Number sending to Fedex

--- rigpl_erpnext/rigpl_erpnext/doctype/carrier_tracking/carrier_tracking.py
+++ rigpl_erpnext/rigpl_erpnext/doctype/carrier_tracking/carrier_tracking.py
@@ -677,6 +677,7 @@ class CarrierTracking(Document):
 		if self.purpose == 'SOLD':
 			po_no = frappe.get_value(self.document, self.document_name, "po_no")
 			po_no = re.sub(r'[^a-zA-Z0-9]', '', po_no)
+			po_no = po_no[:16]
 			si_no = self.document_name
 			for ref, field in {"P_O_NUMBER":po_no, "INVOICE_NUMBER":si_no}.items():
 				ref_data = shipment.create_wsdl_object_of_type('CustomerReference')
