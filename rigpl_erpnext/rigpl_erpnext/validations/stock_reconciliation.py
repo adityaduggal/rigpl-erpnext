ed4d71197843bff7a9a25967cb41ccc5d1d69325
Aditya Duggal <adityaduggal@gmail.com>
2019-04-08 12:27:42 +0530

    Added the following: - SR would now reduce the dead stock VR to 25% of the value

--- rigpl_erpnext/rigpl_erpnext/validations/stock_reconciliation.py
+++ rigpl_erpnext/rigpl_erpnext/validations/stock_reconciliation.py
@@ -6,11 +6,13 @@ from frappe import msgprint
 def validate(doc,method):
 	#Get Stock Valuation from Item Table
 	for d in doc.items:
-		query = """SELECT valuation_rate FROM `tabItem` WHERE name = '%s' """ % d.item_code
+		query = """SELECT valuation_rate, is_purchase_item FROM `tabItem` WHERE name = '%s' """ % d.item_code
 		vr = frappe.db.sql(query, as_list=1)
 		if vr[0][0] != 0 or vr[0][0]:
 			if d.warehouse == "REJ-DEL20A - RIGPL":
 				d.valuation_rate = 1
+			elif d.warehouse == "Dead Stock - RIGPL":
+				d.valuation_rate = (vr[0][0]/4)
 			else:
 				d.valuation_rate = vr[0][0]
 		else:
