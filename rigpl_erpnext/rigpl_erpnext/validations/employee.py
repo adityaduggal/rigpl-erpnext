6a4d9588f8744c179c56fa19b87972bc2960cce5
Aditya Duggal <adityaduggal@gmail.com>
2019-08-08 12:21:57 +0530

    Moved the Validation codes to Utils

--- rigpl_erpnext/rigpl_erpnext/validations/employee.py
+++ rigpl_erpnext/rigpl_erpnext/validations/employee.py
@@ -7,6 +7,7 @@ from frappe import msgprint
 from frappe.utils import getdate
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from rigpl_erpnext.utils.other_utils import validate_pan, validate_aadhaar
 import time
 
 def validate(doc,method):
@@ -46,52 +47,6 @@ def on_update(doc,method):
 	for perm in emp_perm_list:
 		if perm[3] not in allowed_ids:
 			delete_permission(perm[0])
-
-def validate_pan(pan):
-	if pan:
-		p = re.compile("[A-Z]{5}[0-9]{4}[A-Z]{1}")
-		if not p.match(pan):
-			frappe.throw(_("Invalid PAN Number or Enter NA for Unknown"))
-
-def validate_aadhaar(aadhaar):
-	if aadhaar:
-		p = re.compile("[0-9]{12}")
-	if not p.match(aadhaar):
-		frappe.throw(_("Invalid Aadhaar Number"))
-	aadhaar_check_digit = calcsum(aadhaar[:-1])
-	if aadhaar[-1:] != str(aadhaar_check_digit):
-		frappe.throw(_("Invalid Aadhaar Number"))
-
-verhoeff_table_d = (
-    (0,1,2,3,4,5,6,7,8,9),
-    (1,2,3,4,0,6,7,8,9,5),
-    (2,3,4,0,1,7,8,9,5,6),
-    (3,4,0,1,2,8,9,5,6,7),
-    (4,0,1,2,3,9,5,6,7,8),
-    (5,9,8,7,6,0,4,3,2,1),
-    (6,5,9,8,7,1,0,4,3,2),
-    (7,6,5,9,8,2,1,0,4,3),
-    (8,7,6,5,9,3,2,1,0,4),
-    (9,8,7,6,5,4,3,2,1,0))
-
-verhoeff_table_p = (
-    (0,1,2,3,4,5,6,7,8,9),
-    (1,5,7,6,2,8,3,0,9,4),
-    (5,8,0,3,7,9,6,1,4,2),
-    (8,9,1,6,0,4,3,5,2,7),
-    (9,4,5,3,1,2,6,8,7,0),
-    (4,2,8,6,5,7,3,9,0,1),
-    (2,7,9,3,8,0,6,4,1,5),
-    (7,0,4,6,9,1,3,2,5,8))
-
-verhoeff_table_inv = (0,4,3,2,1,5,6,7,8,9)
-
-def calcsum(number):
-    """For a given number returns a Verhoeff checksum digit"""
-    c = 0
-    for i, item in enumerate(reversed(str(number))):
-        c = verhoeff_table_d[c][verhoeff_table_p[(i+1)%8][int(item)]]
-    return verhoeff_table_inv[c]
 	
 def autoname(doc,method):
 	doj = getdate(doc.date_of_joining)
