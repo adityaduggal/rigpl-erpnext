383ed1a3e5732bdcce752f00c50325d4ff4c3d8d
Aditya Duggal <adityaduggal@gmail.com>
2019-08-19 17:18:03 +0530

    Removed RIGPL settings

--- rigpl_erpnext/rigpl_erpnext/doctype/rigpl_settings/rigpl_settings.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, Rohit Industries Ltd. and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Shipway Settings', {
-	refresh: function(frm) {
-
-	}
-});
