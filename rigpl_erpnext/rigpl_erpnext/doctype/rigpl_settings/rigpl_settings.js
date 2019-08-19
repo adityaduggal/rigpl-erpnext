4b7889f3e072b437826ecae9d4cd88eb176b4eec
Aditya Duggal <adityaduggal@gmail.com>
2019-08-19 16:41:50 +0530

    Removed Shipway Settings and Merged into RIGPL Settings Doctype

---
+++ rigpl_erpnext/rigpl_erpnext/doctype/rigpl_settings/rigpl_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Rohit Industries Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('RIGPL Settings', {
+	refresh: function(frm) {
+
+	}
+});
--- rigpl_erpnext/rigpl_erpnext/doctype/rigpl_settings/test_rigpl_settings.js
+++ rigpl_erpnext/rigpl_erpnext/doctype/rigpl_settings/test_rigpl_settings.js
@@ -2,15 +2,15 @@
 // rename this file from _test_[name] to test_[name] to activate
 // and remove above this line
 
-QUnit.test("test: Shipway Settings", function (assert) {
+QUnit.test("test: RIGPL Settings", function (assert) {
 	let done = assert.async();
 
 	// number of asserts
 	assert.expect(1);
 
 	frappe.run_serially([
-		// insert a new Shipway Settings
-		() => frappe.tests.make('Shipway Settings', [
+		// insert a new RIGPL Settings
+		() => frappe.tests.make('RIGPL Settings', [
 			// values to be set
 			{key: 'value'}
 		]),
