1bc68b8a35c6ad3bdba4aa93d4481cec3f63753e
Aditya Duggal <adityaduggal@gmail.com>
2019-08-19 17:08:10 +0530

    Warehouse has some few new fields for Dynamic Items for Production with Validations

--- rigpl_erpnext/rigpl_erpnext/validations/warehouse.py
+++ rigpl_erpnext/rigpl_erpnext/validations/warehouse.py
@@ -13,3 +13,19 @@ def validate(doc,method):
 			frappe.throw(("Warehouse {0} already alloted as Subcontracting Warehouse \
 				and only 1 Subcontracting Warehouse is Allowed").\
 				format(other_subcontracting_warehouses[0][0]))
+	if doc.is_group == 0:
+		if not doc.type_of_warehouse:
+			frappe.throw("Type of Warehouse is Mandatory for {}".format(doc.name))
+		other_serial = frappe.db.sql("""SELECT name FROM `tabWarehouse` 
+			WHERE docstatus=0 AND is_group=0 AND name!='%s' 
+			AND listing_serial = %s"""%(doc.name, doc.listing_serial), as_list=1)
+		if len(other_serial)>0:
+			frappe.throw("Listing Serial: {} already Assigned to Warehouse {}".format(doc.listing_serial, other_serial[0][0]))
+
+		if doc.short_code:
+			oth_short_code = frappe.db.sql("""SELECT name FROM `tabWarehouse` WHERE docstatus=0 AND is_group=0 
+				AND name != '%s' AND short_code = '%s'"""%(doc.name, doc.short_code), as_list=1)
+			if len(oth_short_code)>0:
+				frappe.throw("Short Code: {} already Assigned to Warehouse {}".format(doc.short_code, oth_short_code[0][0]))
+		else:
+			frappe.throw("Short Code is Mandatory for {}".format(doc.name))
