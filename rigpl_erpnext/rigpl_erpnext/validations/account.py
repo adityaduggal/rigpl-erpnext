cd547f382e45b202fde0374dcb15e82ed399444d
Aditya Duggal <adityaduggal@gmail.com>
2020-07-08 12:43:30 +0530

    Accounts Validations checked and refactored

--- rigpl_erpnext/rigpl_erpnext/validations/account.py
+++ rigpl_erpnext/rigpl_erpnext/validations/account.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
-from frappe import msgprint
 from rigpl_erpnext.utils.rigpl_perm import *
 
-def validate(doc,method):
-	if doc.get("__islocal") != 1:
-		copy_users_to_child_accounts(doc)
-		check_account_perm(doc)
+
+def validate(doc, method):
+    if doc.get("__islocal") != 1:
+        copy_users_to_child_accounts(doc)
+        check_account_perm(doc)
