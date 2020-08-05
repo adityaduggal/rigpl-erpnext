3f9b3078db5882ce026b83c9f4a9b327d8c31d00
Aditya Duggal <adityaduggal@gmail.com>
2020-08-05 10:53:10 +0530

    Minor Fix to Indiamart Pull Leads

--- rigpl_erpnext/rigpl_erpnext/doctype/indiamart_pull_leads/indiamart_pull_leads.py
+++ rigpl_erpnext/rigpl_erpnext/doctype/indiamart_pull_leads/indiamart_pull_leads.py
@@ -3,8 +3,11 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+from rigpl_erpnext.rigpl_erpnext.scheduled_tasks.indiamart import execute
 from frappe.model.document import Document
 
+
 class IndiaMartPullLeads(Document):
-	pass
+
+    def get_leads(self):
+        execute()
