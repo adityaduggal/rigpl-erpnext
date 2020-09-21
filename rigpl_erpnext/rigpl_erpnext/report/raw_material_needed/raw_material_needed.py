66c4a5d38bb2696e39a7182e5e4b46fb0d4183e7
Aditya Duggal <adityaduggal@gmail.com>
2020-09-21 16:31:30 +0530

    RM Needed report now has the Production Item Needed quantity for future

--- rigpl_erpnext/rigpl_erpnext/report/raw_material_needed/raw_material_needed.py
+++ rigpl_erpnext/rigpl_erpnext/report/raw_material_needed/raw_material_needed.py
@@ -24,46 +24,48 @@ def get_columns():
 		"Future Stock::100", "Current Stock::100",
 		"Total:Float:50",
 		"RO:Float:40", "SO:Float:40", "PO:Float:40",
-		"PL:Float:40", "IND:Float:40",
+		"PL:Float:40", "IND:Float:50", "PRD:Float:50",
 		"Description::300",
 		"BRM:Float:50", "DRM:Float:50", "BGH:Float:50", "DEL:Float:50",
 		"Dead:Float:50",
 	]
 
 def get_items(filters):
+	actual_data = []
 	conditions_it = get_conditions(filters)
 	bm = filters["bm"]
 	data = frappe.db.sql("""
 	SELECT 
 		it.name,
-		IFNULL(rm.attribute_value, "-"), IFNULL(brand.attribute_value, "-"),
-		IFNULL(quality.attribute_value, "-"), IFNULL(spl.attribute_value, "-"),
-		IFNULL(tt.attribute_value, "-"), 
-		CAST(d1.attribute_value AS DECIMAL(8,3)), 
-		CAST(w1.attribute_value AS DECIMAL(8,3)), 
-		CAST(l1.attribute_value AS DECIMAL(8,3)), 
-		if(ro.warehouse_reorder_level=0, NULL ,ro.warehouse_reorder_level),
-		if(sum(bn.reserved_qty)=0,NULL,sum(bn.reserved_qty)),
-		if(sum(bn.ordered_qty)=0,NULL,sum(bn.ordered_qty)),
-		if(sum(bn.planned_qty)=0,NULL,sum(bn.planned_qty)),
-		if(sum(bn.indented_qty)=0,NULL,sum(bn.indented_qty)),
+		IFNULL(rm.attribute_value, "-") AS rm, IFNULL(brand.attribute_value, "-") AS brand,
+		IFNULL(quality.attribute_value, "-") AS qual, IFNULL(spl.attribute_value, "-") AS spl,
+		IFNULL(tt.attribute_value, "-") AS tt, 
+		CAST(d1.attribute_value AS DECIMAL(8,3)) AS d1, 
+		CAST(w1.attribute_value AS DECIMAL(8,3)) AS w1, 
+		CAST(l1.attribute_value AS DECIMAL(8,3)) AS l1, 
+		if(ro.warehouse_reorder_level=0, NULL ,ro.warehouse_reorder_level) AS rol,
+		if(sum(bn.reserved_qty)=0,NULL,sum(bn.reserved_qty)) AS so,
+		if(sum(bn.ordered_qty)=0,NULL,sum(bn.ordered_qty)) AS po,
+		if(sum(bn.planned_qty)=0,NULL,sum(bn.planned_qty)) AS plan,
+		if(sum(bn.indented_qty)=0,NULL,sum(bn.indented_qty)) AS indent,
+		if(sum(bn.reserved_qty_for_production)=0,NULL,sum(bn.reserved_qty_for_production)) AS prd,
 
 		it.description,	
 		
 		if(min(case WHEN bn.warehouse="RM-BGH655 - RIGPL" THEN bn.actual_qty end)=0,NULL,
-			min(case WHEN bn.warehouse="RM-BGH655 - RIGPL" THEN bn.actual_qty end)),
+			min(case WHEN bn.warehouse="RM-BGH655 - RIGPL" THEN bn.actual_qty end)) AS brm,
 
 		if(min(case WHEN bn.warehouse="RM-DEL20A - RIGPL" THEN bn.actual_qty end)=0,NULL,
-			min(case WHEN bn.warehouse="RM-DEL20A - RIGPL" THEN bn.actual_qty end)),
+			min(case WHEN bn.warehouse="RM-DEL20A - RIGPL" THEN bn.actual_qty end)) AS drm,
 
 		if(min(case WHEN bn.warehouse="BGH655 - RIGPL" THEN bn.actual_qty end)=0,NULL,
-			min(case WHEN bn.warehouse="BGH655 - RIGPL" THEN bn.actual_qty end)),
+			min(case WHEN bn.warehouse="BGH655 - RIGPL" THEN bn.actual_qty end)) AS bgh,
 
 		if(min(case WHEN bn.warehouse="DEL20A - RIGPL" THEN bn.actual_qty end)=0,NULL,
-			min(case WHEN bn.warehouse="DEL20A - RIGPL" THEN bn.actual_qty end)),
+			min(case WHEN bn.warehouse="DEL20A - RIGPL" THEN bn.actual_qty end)) AS del20a,
 
 		if(min(case WHEN bn.warehouse="Dead Stock - RIGPL" THEN bn.actual_qty end)=0,NULL,
-			min(case WHEN bn.warehouse="Dead Stock - RIGPL" THEN bn.actual_qty end))
+			min(case WHEN bn.warehouse="Dead Stock - RIGPL" THEN bn.actual_qty end)) AS dead
 
 	FROM `tabItem` it 
 		LEFT JOIN `tabItem Reorder` ro ON it.name = ro.parent
@@ -100,134 +102,89 @@ def get_items(filters):
 			spl.attribute_value, tt.attribute_value, 
 			CAST(d1.attribute_value AS DECIMAL(8,3)) ASC, 
 			CAST(w1.attribute_value AS DECIMAL(8,3)) ASC, 
-			CAST(l1.attribute_value AS DECIMAL(8,3)) ASC""" % (bm, conditions_it), as_list=1)
-
+			CAST(l1.attribute_value AS DECIMAL(8,3)) ASC""" % (bm, conditions_it), as_dict=1)
 
 	for i in range(0, len(data)):
+		rol = flt(data[i].rol)
+		so = flt(data[i].so)
+		po = flt(data[i].po)
+		plan = flt(data[i].plan)
+		ind = flt(data[i].indent)
+		prd = flt(data[i].prd)
+		brm = flt(data[i].brm)
+		drm = flt(data[i].drm)
+		bgh = flt(data[i].bgh)
+		del20a = flt(data[i].del20a)
+		dead = flt(data[i].dead)
 
-		if data[i][9] is None:
-			ROL=0
-		else:
-			ROL = data[i][9]
+		total = (drm + brm + plan + po + ind + bgh + del20a + dead) - prd
 
-		if data[i][10] is None:
-			SO=0
-		else:
-			SO = data[i][10]
-
-		if data[i][11] is None:
-			PO=0
-		else:
-			PO = data[i][11]
-
-		if data[i][12] is None:
-			PLAN=0
-		else:
-			PLAN = data[i][12]
-			
-		if data[i][13] is None:
-			IND=0
-		else:
-			IND = data[i][13]
-
-		if data[i][15] is None:
-			BRM=0
-		else:
-			BRM = data[i][15]
-
-		if data[i][16] is None:
-			DRM=0
-		else:
-			DRM = data[i][16]
-
-		if data[i][17] is None:
-			BGH=0
-		else:
-			BGH = data[i][17]
-
-		if data[i][18] is None:
-			DEL=0
-		else:
-			DRM = data[i][18]
-
-		if data[i][19] is None:
-			Dead=0
-		else:
-			Dead = data[i][19]
-
-		total = (DRM + BRM + PLAN + PO + IND + BGH + DEL + Dead)
-
-		stock = DRM + BRM + BGH + DEL + Dead
+		stock = drm + brm + bgh + del20a + dead - prd
 		prod = total - stock
 		fut_stock = "X"
 
-		if ROL <10:
-			ROL = 3*ROL
-		elif 10<=ROL < 20:
-			ROL = 2*ROL
-		elif 20<=ROL<50:
-			ROL = 1.5*ROL
+		if rol <10:
+			rol = 3*rol
+		elif 10 <= rol < 20:
+			rol = 2*rol
+		elif 20 <= rol < 50:
+			rol = 1.5*rol
 
-		if total < SO:
+		if total < so:
 			fut_stock = "Raise More PO and Indent"
-		elif total < SO + ROL:
+		elif total < so + rol:
 			fut_stock = "1<30 Days"
-		elif total < SO + 2*ROL:
+		elif total < so + 2*rol:
 			fut_stock = "2<60 Days"
-		elif total < SO + 3*ROL:
+		elif total < so + 3*rol:
 			fut_stock = "3<90 Days"
-		elif total < SO + 4*ROL:
+		elif total < so + 4*rol:
 			fut_stock = "4<120 Days"
-		elif total < SO + 5*ROL:
+		elif total < so + 5*rol:
 			fut_stock = "5<150 Days"
-		elif total < SO + 6*ROL:
+		elif total < so + 6*rol:
 			fut_stock = "6<180 Days"
-		elif total > SO + 6*ROL:
-			if ROL >0:
+		elif total > so + 6*rol:
+			if rol >0:
 				fut_stock = "7 Over Stocked >180 Days"
 			else:
 				fut_stock = "-"
 		else:
 			fut_stock = "-"
 			
-		if stock < SO:
+		if stock < rol:
 			cur_stock = "NO STOCK"
-		elif stock < SO + ROL:
+		elif stock < so + rol:
 			cur_stock = "1<30 Days"
-		elif stock < SO + 2*ROL:
+		elif stock < so + 2*rol:
 			cur_stock = "2<60 Days"
-		elif stock < SO + 3*ROL:
+		elif stock < so + 3*rol:
 			cur_stock = "3<90 Days"
-		elif stock < SO + 4*ROL:
+		elif stock < so + 4*rol:
 			cur_stock = "4<120 Days"
-		elif stock < SO + 5*ROL:
+		elif stock < so + 5*rol:
 			cur_stock = "5<150 Days"
-		elif stock < SO + 6*ROL:
+		elif stock < so + 6*rol:
 			cur_stock = "6<180 Days"
-		elif stock > SO + 6*ROL:
-			if ROL >0:
+		elif stock > so + 6*rol:
+			if rol >0:
 				cur_stock = "7 Over Stocked >180 Days"
 			else:
 				cur_stock = "-"
 		else:
 			cur_stock = "-"
 
-		data[i].insert (9, fut_stock)
-		data[i].insert(10, cur_stock)
-		data[i].insert (11, total)
+		row = [
+			data[i].name, data[i].rm, data[i].brand, data[i].qual, data[i].spl, data[i].tt, data[i].d1,
+			data[i].w1, data[i].l1, fut_stock, cur_stock, total, rol, so, po, plan, ind, prd, data[i].description,
+			brm, drm, bgh, del20a, dead
+		]
+		for i in range(0, len(row)):
+			if row[i] == 0:
+				row[i] = None
+		actual_data.append(row)
 
-	for j in range(0,len(data)):
-		for k in range(0, len(data[j])):
-			if data[j][k] ==0:
-				data[j][k] = None
-				
-	attributes = ['Is RM', 'Brand', '%Quality', 'Special Treatment',
-	'Tool Type', 'Material to Machine', 'Purpose', 'Type Selector',
-	'd1_mm', 'w1_mm', 'l1_mm']
-	
-	float_fields = ['d1_mm', 'w1_mm', 'l1_mm']
-	
-	return data
+	return actual_data
 	
 def get_conditions(filters):
 	conditions_it = ""
