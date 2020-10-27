372eb7b4d11694bc9a71c988d26b38f37ddd3335
Aditya Duggal <adityaduggal@gmail.com>
2020-10-27 17:29:50 +0530

    Fix now the Update Job Card status is working flawlessly.

--- rigpl_erpnext/rigpl_erpnext/scheduled_tasks/update_job_card_status.py
+++ rigpl_erpnext/rigpl_erpnext/scheduled_tasks/update_job_card_status.py
@@ -18,8 +18,20 @@ def execute():
     for jc in jc_dict:
         jc_doc = frappe.get_doc("Process Job Card RIGPL", jc.name)
         ps_doc = frappe.get_doc("Process Sheet", jc_doc.process_sheet)
+        old_qty_available = jc_doc.qty_available
+        old_status = jc_doc.status
         update_job_card_qty_available(jc_doc)
         update_job_card_status(jc_doc)
+        new_qty_available = jc_doc.qty_available
+        new_status = jc_doc.status
+        if old_qty_available != new_qty_available or old_status != new_status:
+            updated_jc_nos += 1
+            jc_doc.save()
+            print("Updated Job Card # {}".format(jc_doc.name))
+            if updated_jc_nos % 10 == 0 and updated_jc_nos > 0:
+                frappe.db.commit()
+                print("Committing Changes after {} Nos JC Updation".format(updated_jc_nos))
     end_time = time.time()
     print("Total Number of Job Cards in Draft: " + str(len(jc_dict)))
+    print("Total Number of Job Cards Updated: " + str(updated_jc_nos))
     print(f"Total Execution Time: {end_time - start_time} seconds")
