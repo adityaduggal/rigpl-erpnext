9761b692d2149580cdc909a3a9d755b01d1a68b6
Aditya Duggal <adityaduggal@gmail.com>
2020-10-29 20:47:22 +0530

    Pending Purchase orders now showing

--- rigpl_erpnext/rigpl_erpnext/report/pending_purchase_orders/pending_purchase_orders.py
+++ rigpl_erpnext/rigpl_erpnext/report/pending_purchase_orders/pending_purchase_orders.py
@@ -17,8 +17,8 @@ def get_columns(filters):
 		"PO# :Link/Purchase Order:120", "PO Date:Date:80", 
 		"SCH Date:Date:80", "Supplier:Link/Supplier:200", "Item Code:Link/Item:120", 
 		"Description::280", "Pend Qty:Float:60", "Ordered Qty:Float:60", "Rejected Qty:Float:60",
-		"UoM::50", "Price:Currency:80", "Sub Contract Item Code:Link/Item:120", 
-		"Item Qty:Float:100", "Stock Entry:Link/Stock Entry:150"
+		"UoM::50", "Price:Currency:80",
+		"Item Qty:Float:100", "JCR:Link/Process Job Card RIGPL:100"
 		]
 	else:
 		return[
@@ -31,13 +31,12 @@ def get_columns(filters):
 def get_items(filters, conditions):
 	if filters.get("subcontracting") == 1:
 		query = """SELECT po.name, po.transaction_date, pod.schedule_date, po.supplier, 
-			pod.item_code, pod.description, (pod.qty - pod.received_qty), 
-			pod.qty, pod.returned_qty, pod.stock_uom, pod.base_rate, pod.subcontracted_item,
-			pod.conversion_factor, ste.name
-			FROM `tabPurchase Order` po, `tabPurchase Order Item` pod, `tabStock Entry` ste
-			WHERE po.docstatus = 1 AND po.name = pod.parent AND ste.purchase_order = po.name
-				AND po.status != 'Closed' AND IFNULL(pod.received_qty,0) < IFNULL(pod.qty,0)
-				%s
+			pod.subcontracted_item, pod.description, (pod.qty - pod.received_qty), 
+			pod.qty, pod.returned_qty, pod.stock_uom, pod.base_rate,
+			pod.conversion_factor, pod.reference_dn
+			FROM `tabPurchase Order` po, `tabPurchase Order Item` pod
+			WHERE po.docstatus = 1 AND po.name = pod.parent AND po.status != 'Closed' 
+			AND IFNULL(pod.received_qty,0) < IFNULL(pod.qty,0) %s
 			ORDER BY po.transaction_date, pod.schedule_date""" %conditions
 	else:
 		query = """SELECT po.name, po.transaction_date, pod.schedule_date, po.supplier, 
