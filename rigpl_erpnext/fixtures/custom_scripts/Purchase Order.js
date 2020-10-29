2aefb26854463baa2763fb71f843510f9c5819d8
Aditya Duggal <adityaduggal@gmail.com>
2020-10-29 20:02:38 +0530

    Minor Upgrade to how Item is fetched in PO from JC

--- rigpl_erpnext/fixtures/custom_scripts/Purchase Order.js
+++ rigpl_erpnext/fixtures/custom_scripts/Purchase Order.js
@@ -11,16 +11,18 @@ frappe.ui.form.on("Purchase Order Item", {
 	}
 });
 
+/*
 frappe.ui.form.on("Purchase Order Item", "item_code", function(frm, cdt, cdn){
     var d = locals[cdt][cdn];
     description = get_description();
     d.description = description;
     frm.refresh_fields();
 });
+*/
 
 cur_frm.cscript.is_subcontracting = function(doc, cdt, cdn) {
     cur_frm.set_query("item_code", "items", function(){
-        if (cur_frm.doc.is_subcontracting == 1){
+        if (cur_frm.doc.is_subcontracting === 1){
             return{
                 query: "erpnext.controllers.queries.item_query",
                 filters:{ 'is_job_work': 1 }
@@ -79,23 +81,39 @@ cur_frm.add_custom_button(__('Job Card'),
             },
             callback: function(res){
             if(res && res.message){
-                    console.log(res.message[0])
-                    var row = frappe.model.add_child(cur_frm.doc, "Purchase Order Item", "items");
-                    row.qty = res.message[0]['qty_available'];
-                    if (cur_frm.doc.is_subcontracting == 1){
-                        row.subcontracted_item = res.message[0]['production_item'];
-                    } else{
-                        row.item_code = res.message[0]['production_item'];
-                    }
-                    row.reference_dt = "Process Job Card RIGPL"
-                    row.reference_dn = res.message[0]['name'];
-                    row.description = res.message[0]['description'];
-                    row.from_warehouse = res.message[0]['s_warehouse']
-                    row.so_detail = res.message[0]['sales_order_item'];
-                    row.uom = res.message[0]['uom'];
-                    row.stock_uom = res.message[0]['uom'];
-                    row.conversion_factor = 1;
+                var today = new Date();
+                var row = frappe.model.add_child(cur_frm.doc, "Purchase Order Item", "items");
+                var jc_name =  res.message[0]['name']
+                var operation = frappe.model.get_value('Process Job Card RIGPL', jc_name, 'operation');
+                var subcon_item_code = frappe.model.get_value('Operation', operation, 'sub_contracting_item_code');
+                frm.doc.is_subcontracting = 1;
+                refresh_field("is_subcontracting");
+                console.log(subcon_item_code);
+                console.log(operation);
+                row.qty = res.message[0]['qty_available'];
+                if (cur_frm.doc.is_subcontracting == 1){
+                    row.subcontracted_item = res.message[0]['production_item'];
+                    row.item_code = subcon_item_code
+                } else{
+                    row.item_code = res.message[0]['production_item'];
+                }
+                row.reference_dt = "Process Job Card RIGPL"
+                row.reference_dn = res.message[0]['name'];
+                row.description = res.message[0]['description'];
+                row.from_warehouse = res.message[0]['s_warehouse']
+                row.so_detail = res.message[0]['sales_order_item'];
+                row.price_list_rate = 0;
+                row.rate = 0;
+                if (frm.doc.schedule_date > today){
+                    row.schedule_date = frm.doc.schedule_date
+                } else {
+                    row.schedule_date = today
+                }
+                row.uom = res.message[0]['uom'];
+                row.stock_uom = res.message[0]['uom'];
+                row.conversion_factor = 1;
                 refresh_field("items");
+
             }
         }
     });
@@ -115,72 +133,6 @@ cur_frm.add_custom_button(__('Job Card'),
             }
             dialog.hide()
         }    
-        else{
-            msgprint("Select Item Before Add")
-        }
-    }
-}, __("Get items from"));
-cur_frm.add_custom_button(__('Work Order'),
-    function() {
-        var dialog = new frappe.ui.Dialog({
-        title: "Get Items from Work Order",
-        fields: [
-            {"fieldtype": "Link", "label": __("Work Order"), "fieldname": "production_order","options":'Work Order',"reqd": 1,
-                get_query: function() { return {query: "rigpl_erpnext.rigpl_erpnext.validations.purchase_order.get_pending_prd"}},
-            },
-            {"fieldtype": "Button", "label": __("Get"), "fieldname": "get"},
-            {"fieldtype": "HTML", "fieldname": "production_order_items_details"},
-        ],
-    });
-    dialog.show();
-    var fd = dialog.fields_dict;
-    dialog.fields_dict.get.$input.click(function() {
-        var value = dialog.get_values();
-
-    frappe.call({
-        method: "frappe.client.get_list",
-           args: {
-            doctype: "Work Order",
-               fields: ["production_item", "item_description ", "sales_order", "so_detail", "qty","fg_warehouse","stock_uom","name"],
-               filters: { "name" : value.production_order
-                },
-            },
-            callback: function(res){
-            if(res && res.message){
-                    var row = frappe.model.add_child(cur_frm.doc, "Purchase Order Item", "items");
-                    row.qty = res.message[0]['qty'];
-                    if (cur_frm.doc.is_subcontracting == 1){
-                        row.subcontracted_item = res.message[0]['production_item'];
-                    } else{
-                        row.item_code = res.message[0]['production_item'];
-                    }
-                    row.reference_dt = "Work Order"
-                    row.reference_dn = value.production_order
-                    row.description = res.message[0]['item_description'];
-                    row.so_detail = res.message[0]['so_detail'];
-                    row.uom = res.message[0]['stock_uom'];
-                    row.stock_uom = res.message[0]['stock_uom'];
-                    row.conversion_factor = 1;
-                refresh_field("items");
-            }
-        }
-    });
-
-
-    });
-
-    var add_production_order_items_to_stock = function(){
-        var items_to_add = []
-        var value = dialog.get_values();
-        $.each($(fd.production_order_items_details.wrapper).find(".select:checked"), function(name, item){
-            items_to_add.push($(item).val());
-        });
-        if(items_to_add.length > 0){
-            for(i=0;i<items_to_add.length;i++){
-                add_production_order_items(items_to_add,i)
-            }
-            dialog.hide()
-        }
         else{
             msgprint("Select Item Before Add")
         }
