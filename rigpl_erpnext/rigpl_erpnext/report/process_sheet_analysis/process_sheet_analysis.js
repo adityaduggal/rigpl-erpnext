b26fccda161c3cc74956b158e78da80ccfa8e3c4
Aditya Duggal <adityaduggal@gmail.com>
2020-11-06 13:53:40 +0530

    Process Sheet Analysis Report is now working

--- rigpl_erpnext/rigpl_erpnext/report/process_sheet_analysis/process_sheet_analysis.js
+++ rigpl_erpnext/rigpl_erpnext/report/process_sheet_analysis/process_sheet_analysis.js
@@ -5,40 +5,19 @@
 frappe.query_reports["Process Sheet Analysis"] = {
 	"filters": [
 		{
-			"fieldname":"rm",
-			"label": "Is RM",
-			"fieldtype": "Link",
-			"options": "Item Attribute Value",
-			"ignore_link_validation": true,
-			"get_query": function(){ return {query: "rigpl_erpnext.utils.attribute_query.attribute_rm_query"}}
+			"fieldname":"status",
+			"label": "Status",
+			"fieldtype": "Select",
+			"options": "\nCancelled\nCompleted\nDraft\nIn Progress\nShort Closed\nStopped\nSubmitted"
 		},
 		{
 			"fieldname":"bm",
 			"label": "Base Material",
 			"fieldtype": "Link",
 			"options": "Item Attribute Value",
-			"reqd": 1,
+			"reqd": 0,
 			"ignore_link_validation": true,
 			"get_query": function(){ return {query: "rigpl_erpnext.utils.attribute_query.attribute_bm_query"}}
-		},
-		{
-			"fieldname":"brand",
-			"label": "Brand",
-			"fieldtype": "Link",
-			"options": "Item Attribute Value",
-			"reqd": 0,
-			"ignore_link_validation": true,
-			"get_query": function(){ return {query: "rigpl_erpnext.utils.attribute_query.attribute_brand_query"}}
-		},
-
-		{
-			"fieldname":"quality",
-			"label": "Quality",
-			"fieldtype": "Link",
-			"options": "Item Attribute Value",
-			"reqd": 0,
-			"ignore_link_validation": true,
-			"get_query": function(){ return {query: "rigpl_erpnext.utils.attribute_query.attribute_quality_query"}}
 		},
 		{
 			"fieldname":"tt",
@@ -50,6 +29,14 @@ frappe.query_reports["Process Sheet Analysis"] = {
 			"get_query": function(){ return {query: "rigpl_erpnext.utils.attribute_query.attribute_tt_query"}}
 		},
 		{
+			"fieldname":"series",
+			"label": "Series",
+			"fieldtype": "Link",
+			"options": "Item Attribute Value",
+			"ignore_link_validation": true,
+			"get_query": function(){ return {query: "rigpl_erpnext.utils.attribute_query.attribute_series_query"}}
+		},
+		{
 			"fieldname":"spl",
 			"label": "Special Treatment",
 			"fieldtype": "Link",
@@ -57,6 +44,22 @@ frappe.query_reports["Process Sheet Analysis"] = {
 			"reqd": 0,
 			"ignore_link_validation": true,
 			"get_query": function(){ return {query: "rigpl_erpnext.utils.attribute_query.attribute_spl_query"}}
+		},
+		{
+			"fieldname":"item",
+			"label": "Item",
+			"fieldtype": "Link",
+			"options": "Item",
+			"reqd": 0,
+			"get_query": function(){ return {'filters': [['Item', 'has_variants','=', 0]]}}
+		},
+		{
+			"fieldname":"so",
+			"label": "Sales Order",
+			"fieldtype": "Link",
+			"options": "Sales Order",
+			"reqd": 0,
+			"get_query": function(){ return {'filters': [['Sales Order', 'docstatus','=', 1]]}}
 		}
 	]
 };
--- rigpl_erpnext/rigpl_erpnext/report/process_sheet_analysis/process_sheet_analysis.py
+++ rigpl_erpnext/rigpl_erpnext/report/process_sheet_analysis/process_sheet_analysis.py
@@ -6,126 +6,94 @@ import frappe
 
 
 def execute(filters=None):
-    bm = filters.get("bm")
-    conditions_it = get_conditions(bm, filters)
     columns = get_columns(filters)
-
-    data = get_data(conditions_it, filters)
+    data = get_data(filters)
     return columns, data
 
 
 def get_columns(filters):
     columns = [
-        "Process Sheet:Link/Process Sheet:100", "Item:Link/Item:100", "BM::100", "TT::100", "SPL::100", "Qual::100",
-        "Brand::100", "D1:Float:80", "W1:Float:80", "L1:Float:80", "D2:Float:80", "L2:Float:80", "Qty:Float:80",
-        "Completed Qty:Float:80", "Status::100", "BT:Link/BOM Template RIGPL:100"
+        "Process Sheet:Link/Process Sheet:100", "Status::80", "Item:Link/Item:120",
+        "BM::60", "TT::60", "SPL::60", "Series::40",
+        "D1:Float:50", "W1:Float:50", "L1:Float:50", "D2:Float:50", "L2:Float:50", "Qty:Int:50",
+        "Comp Qty:Int:50", "SC Qty:Int:50", "BT:Link/BOM Template RIGPL:80",
+        "ROL:Int:50", "SO:Int:50", "PO:Int:50", "Plan:Int:50", "PRD:Int:50", "ACT:Int:50",
+        "Description::500"
     ]
     return columns
 
 
-def get_data(conditions_it, filters):
-    bm = filters.get("bm")
-    query_join = get_joins(bm=bm, filters=filters)
-    frappe.msgprint(conditions_it)
-    frappe.msgprint(query_join)
-    query = """SELECT ps.name, ps.production_item, bm.attribute_value, tt.attribute_value, spl.attribute_value, 
-    qual.attribute_value, brand.attribute_value, d1.attribute_value, w
-    bt.formula
-        FROM `tabBOM Template RIGPL` bt %s %s
-        ORDER BY %s bt.name""" % (query_join, conditions_it)
-    frappe.throw(query)
+def get_data(filters):
+    conditions_it, conditions_ps = get_conditions (filters)
+    query = """SELECT ps.name, ps.status, ps.production_item, bm.attribute_value, tt.attribute_value,
+    spl.attribute_value, ser.attribute_value, d1.attribute_value, w1.attribute_value, l1.attribute_value,
+    d2.attribute_value, l2.attribute_value, ps.quantity, IF(ps.produced_qty=0, NULL,ps.produced_qty), 
+    IF(ps.short_closed_qty=0, NULL, ps.short_closed_qty), 
+    ps.bom_template,
+    IF(ro.warehouse_reorder_level=0, NULL ,ro.warehouse_reorder_level) AS rol,
+    IF(bn.on_so=0, NULL ,bn.on_so) AS on_so,
+    IF(bn.on_po=0, NULL ,bn.on_so) AS on_po,
+    IF(bn.plan=0, NULL ,bn.plan) AS plan,
+    IF(bn.prod=0, NULL ,bn.prod) AS prod,
+    IF(bn.act=0, NULL ,bn.act) AS act, ps.description
+    FROM `tabProcess Sheet` ps 
+    LEFT JOIN `tabItem` it ON it.name = ps.production_item
+    LEFT JOIN `tabItem Reorder` ro ON ps.production_item = ro.parent
+    LEFT JOIN (SELECT item_code, SUM(reserved_qty) as on_so, SUM(ordered_qty) as on_po, SUM(actual_qty) as act,
+    	SUM(planned_qty) as plan, SUM(reserved_qty_for_production) as prod, SUM(indented_qty) as indent
+    	FROM `tabBin` GROUP BY item_code) bn 
+    	ON ps.production_item = bn.item_code
+    LEFT JOIN `tabItem Variant Attribute` bm ON it.name = bm.parent
+    	AND bm.attribute = 'Base Material'
+    LEFT JOIN `tabItem Variant Attribute` tt ON it.name = tt.parent
+    	AND tt.attribute = 'Tool Type'
+    LEFT JOIN `tabItem Variant Attribute` spl ON it.name = spl.parent
+    	AND spl.attribute = 'Special Treatment'
+    LEFT JOIN `tabItem Variant Attribute` ser ON it.name = ser.parent
+    	AND ser.attribute = 'Series'
+    LEFT JOIN `tabItem Variant Attribute` d1 ON it.name = d1.parent
+    	AND d1.attribute = 'd1_mm'
+    LEFT JOIN `tabItem Variant Attribute` w1 ON it.name = w1.parent
+    	AND w1.attribute = 'w1_mm'
+    LEFT JOIN `tabItem Variant Attribute` l1 ON it.name = l1.parent
+    	AND l1.attribute = 'l1_mm'
+    LEFT JOIN `tabItem Variant Attribute` d2 ON it.name = d2.parent
+    	AND d2.attribute = 'd2_mm'
+    LEFT JOIN `tabItem Variant Attribute` l2 ON it.name = l2.parent
+    	AND l2.attribute = 'l2_mm'
+    WHERE ps.docstatus != 3 %s %s
+    ORDER BY bm.attribute_value, tt.attribute_value, spl.attribute_value, ser.attribute_value, d1.attribute_value,
+    w1.attribute_value, l1.attribute_value, d2.attribute_value, l2.attribute_value,
+    ps.production_item, ps.sales_order, ps.description""" % (conditions_ps, conditions_it)
     data = frappe.db.sql(query, as_list=1)
     return data
 
 
-def define_join(string, table_name, allowed_values):
-    string += """ LEFT JOIN `tabItem Variant Restrictions` %s ON bt.name = %s.parent AND %s.attribute = '%s' AND 
-    %s.parentfield = 'fg_restrictions' AND %s.parenttype = 'BOM Template RIGPL'""" % \
-              (table_name, table_name, table_name, table_name, table_name, allowed_values)
-    return string
-
-
-def get_joins(bm, filters):
-    query_join = ""
-    if filters.get("rm"):
-        tab = 'Is RM'
-        query_join = define_join(query_join, tab.replace(" ", ""), tab)
-
-    if filters.get("bm"):
-        tab = 'Base Material'
-        query_join = define_join(query_join, tab.replace(" ", ""), tab)
-
-    if filters.get("tt"):
-        tab = 'Tool Type'
-        query_join = define_join(query_join, tab.replace(" ", ""), tab)
-
-    if filters.get("quality"):
-        tab = '%s Quality' % (bm)
-        query_join = define_join(query_join, tab.replace(" ", ""), tab)
-
-    if filters.get("series"):
-        tab = 'Series'
-        query_join = define_join(query_join, tab.replace(" ", ""), tab)
-
-    if filters.get("spl"):
-        tab = 'Special Treatment'
-        query_join = define_join(query_join, tab.replace(" ", ""), tab)
-
-    if filters.get("purpose"):
-        tab = 'Purpose'
-        query_join = define_join(query_join, tab.replace(" ", ""), tab)
-
-    if filters.get("type"):
-        tab = 'Type Selector'
-        query_join = define_join(query_join, tab.replace(" ", ""), tab)
-
-    if filters.get("mtm"):
-        tab = 'Material to Machine'
-        query_join = define_join(query_join, tab.replace(" ", ""), tab)
-
-    return query_join
-
-
-def get_conditions(bm, filters):
+def get_conditions(filters):
     conditions_it = ""
+    conditions_ps = ""
 
-    if filters.get("eol"):
-        conditions_it += " WHERE IFNULL(it.end_of_life, '2099-12-31') > '%s'" % filters.get("eol")
-
-    if filters.get("rm"):
-        conditions_it += " AND IsRM.attribute_value = '%s'" % filters.get("rm")
+    if filters.get("status"):
+        conditions_ps += " AND ps.status = '%s'" % filters.get("status")
+    else:
+        conditions_ps += " AND ps.docstatus = 1 AND ps.status != 'Stopped' AND ps.status != 'Completed'"
 
     if filters.get("bm"):
-        conditions_it += " AND BaseMaterial.attribute_value = '%s'" % filters.get("bm")
-
-    if filters.get("series"):
-        conditions_it += " AND Series.attribute_value = '%s'" % filters.get("series")
-
-    if filters.get("quality"):
-        conditions_it += " AND %sQuality.attribute_value = '%s'" % (bm, filters.get("quality"))
-
-    if filters.get("spl"):
-        conditions_it += " AND SpecialTreatment.attribute_value = '%s'" % filters.get("spl")
-
-    if filters.get("purpose"):
-        conditions_it += " AND Purpose.attribute_value = '%s'" % filters.get("purpose")
-
-    if filters.get("type"):
-        conditions_it += " AND TypeSelector.attribute_value = '%s'" % filters.get("type")
-
-    if filters.get("mtm"):
-        conditions_it += " AND MaterialtoMachine.attribute_value = '%s'" % filters.get("mtm")
+        conditions_it += " AND bm.attribute_value = '%s'" % filters.get("bm")
 
     if filters.get("tt"):
-        conditions_it += " AND ToolType.attribute_value = '%s'" % filters.get("tt")
+        conditions_it += " AND tt.attribute_value = '%s'" % filters.get("tt")
 
-    if filters.get("show_in_website") == 1:
-        conditions_it += " and it.show_variant_in_website =%s" % filters.get("show_in_website")
+    if filters.get("series"):
+        conditions_it += " AND ser.attribute_value = '%s'" % filters.get("series")
+
+    if filters.get("spl"):
+        conditions_it += " AND spl.attribute_value = '%s'" % filters.get("spl")
 
     if filters.get("item"):
         conditions_it += " and it.name = '%s'" % filters.get("item")
 
-    if filters.get("variant_of"):
-        conditions_it += " and it.variant_of = '%s'" % filters.get("variant_of")
+    if filters.get("so"):
+        conditions_it += " and ps.sales_order = '%s'" % filters.get("so")
 
-    return conditions_it
+    return conditions_it, conditions_ps
