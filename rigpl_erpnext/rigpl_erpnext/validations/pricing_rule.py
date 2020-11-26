61376f5c25cd36faea869c96efee395cd2e47034
Aditya Duggal <adityaduggal@gmail.com>
2020-11-26 13:26:42 +0530

    fix: Pricing Rule validations would now add UOM automatically and priority if not selected

--- rigpl_erpnext/rigpl_erpnext/validations/pricing_rule.py
+++ rigpl_erpnext/rigpl_erpnext/validations/pricing_rule.py
@@ -4,7 +4,12 @@ import frappe
 
 
 def validate(doc, method):
+    if not doc.priority:
+        doc.priority = 1
     if doc.apply_on == 'Attributes':
+        doc.items = []
+        doc.item_groups = []
+        doc.brands = []
         for att in doc.attributes:
             att_doc = frappe.get_doc('Item Attribute', att.attribute)
             att.is_numeric = att_doc.numeric_values
@@ -16,3 +21,26 @@ def validate(doc, method):
         doc.rate_or_discount = 'Rate'
         if doc.rate == 0 or not doc.rate:
             frappe.throw("Rate is Mandatory for Attribute Based Pricing Rule")
+    elif doc.apply_on == "Item Code":
+        doc.attribute = []
+        doc.item_groups = []
+        doc.brands = []
+        for it in doc.items:
+            if not it.uom:
+                it.uom = frappe.get_value("Item", it.item_code, "stock_uom")
+    elif doc.apply_on == "Item Group":
+        doc.attribute = []
+        doc.items = []
+        doc.brands = []
+        for it in doc.item_groups:
+            if not it.uom:
+                it.uom = "Nos"
+    else:
+        frappe.msgprint(f"For Pricing Rule {doc.name} Blocked {doc.apply_on}")
+    if doc.for_price_list:
+        doc.currency = frappe.get_value("Price List", doc.for_price_list, "currency")
+    if doc.rate_or_discount:
+        if doc.margin_type:
+            doc.margin_type = ""
+        if doc.margin_rate_or_amount > 0:
+            doc.marging_rate_or_amount = 0
