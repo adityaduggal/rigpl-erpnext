82d2311b50ed1728be57ff994827bd3dd6af0ddc
Aditya Duggal <adityaduggal@gmail.com>
2021-04-18 15:09:15 +0530

    fix: Moved the Homepage section and settings from RIGPL theme to RIGPL Erpnext

---
+++ rigpl_erpnext/rigpl_erpnext/doctype/homepage_settings/homepage_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Rohit Industries Group Private Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Homepage Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
