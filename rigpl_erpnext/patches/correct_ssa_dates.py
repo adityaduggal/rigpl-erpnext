d6efa7cbf8792646b0a93f93b32262579b2b46e1
Aditya Duggal <adityaduggal@gmail.com>
2021-06-28 11:44:11 +0530

    Added: Patch to correct the to date for SSA

---
+++ rigpl_erpnext/patches/correct_ssa_dates.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import frappe
+from ..rigpl_erpnext.validations.salary_structure_assignment import correct_to_date_for_ssa
+
+
+def execute():
+    emp_list = frappe.db.sql("""SELECT name, employee_name, date_of_joining FROM `tabEmployee` ORDER BY date_of_joining""", as_dict=1)
+    for emp in emp_list:
+        print(f"Checking for {emp.employee_name} with ID: {emp.name}")
+        correct_to_date_for_ssa(emp.name)
