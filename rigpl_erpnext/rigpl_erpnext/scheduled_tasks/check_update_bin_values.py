bcf5de06d442784c8501e1b142a85711b628a959
Aditya Duggal <adityaduggal@gmail.com>
2021-09-14 05:26:30 +0530

    fix: changes made the tasks was not running due to async = false hence changed

--- rigpl_erpnext/rigpl_erpnext/scheduled_tasks/check_update_bin_values.py
+++ rigpl_erpnext/rigpl_erpnext/scheduled_tasks/check_update_bin_values.py
@@ -13,34 +13,40 @@ from frappe.utils.background_jobs import enqueue
 
 
 def enqueue_ex():
-    enqueue(execute, queue="long", timeout=1500, is_async=False)
+    enqueue(execute, queue="long", timeout=1500)
 
 
 def execute():
     st_time = time.time()
-    it_list = frappe.db.sql("""SELECT name FROM `tabItem` WHERE disabled=0 AND made_to_order = 0 
-    AND is_stock_item = 1 AND has_variants = 0""", as_dict=1)
+    it_list = frappe.db.sql("""SELECT it.name, bn.reserved_qty, bn.planned_qty
+        FROM `tabItem` it, `tabBin` bn
+        WHERE it.disabled = 0 AND it.made_to_order = 0 AND bn.item_code = it.name
+        AND it.is_stock_item = 1 AND it.has_variants = 0 AND (bn.reserved_qty > 0 or
+        bn.planned_qty > 0)
+        GROUP BY it.name
+        ORDER BY bn.reserved_qty DESC, bn.planned_qty DESC, it.name ASC""", as_dict=1)
     sno = 0
+    print(f"Total Items to be Checked = {len(it_list)}")
     for it in it_list:
         sno += 1
         pending_so_dict = get_total_pending_so_item(it.name)
         bin_data = get_consolidate_bin(it.name)
         plan_data = get_planned_qty(it.name)
         if flt(plan_data.planned) != flt(bin_data[0].planned):
-            print(f"{it.name} Planned Data {plan_data.planned} But BIN Data {bin_data[0].planned}")
-            bin_name = frappe.db.sql("""SELECT name FROM `tabBin` WHERE item_code = '%s' 
+            bin_name = frappe.db.sql("""SELECT name FROM `tabBin` WHERE item_code = '%s'
             AND planned_qty = %s""" % (it.name, bin_data[0].planned), as_list=1)
             if bin_name:
+                print(f"{it.name} Planned Data {plan_data.planned} But BIN Data "
+                    f"{bin_data[0].planned}")
                 frappe.db.set_value("Bin", bin_name[0][0], "planned_qty", plan_data.planned)
             else:
                 print(f"{it.name} BIN Missing")
-
-        if flt(bin_data[0].on_so) != flt(pending_so_dict[0].pending_qty):
-            print(f"For {it.name} Values on SO {pending_so_dict[0].pending_qty} whereas on "
-                  f"BIN {bin_data[0].on_so}")
-            bin_name = frappe.db.sql("""SELECT name FROM `tabBin` WHERE item_code = '%s' 
+        elif flt(bin_data[0].on_so) != flt(pending_so_dict[0].pending_qty):
+            bin_name = frappe.db.sql("""SELECT name FROM `tabBin` WHERE item_code = '%s'
             AND reserved_qty = %s""" % (it.name, bin_data[0].on_so), as_list=1)
             if bin_name:
+                print(f"For {it.name} Values on SO {pending_so_dict[0].pending_qty} whereas on "
+                    f"BIN {bin_data[0].on_so}")
                 frappe.db.set_value("Bin", bin_name[0][0], "reserved_qty", pending_so_dict[0].pending_qty)
             else:
                 print(f"For {it.name} Values on SO {pending_so_dict[0].pending_qty} whereas NO BIN")
