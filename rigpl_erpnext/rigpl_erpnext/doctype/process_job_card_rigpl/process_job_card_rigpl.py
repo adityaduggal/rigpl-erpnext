a1c4a05aabe57b7679fd2c2601e4321b2f90edb4
Aditya Duggal <adityaduggal@gmail.com>
2021-09-15 14:19:46 +0530

    fix: Made changes to PEP

--- rigpl_erpnext/rigpl_erpnext/doctype/process_job_card_rigpl/process_job_card_rigpl.py
+++ rigpl_erpnext/rigpl_erpnext/doctype/process_job_card_rigpl/process_job_card_rigpl.py
@@ -14,33 +14,45 @@ from ....utils.process_sheet_utils import update_process_sheet_operations
 
 class ProcessJobCardRIGPL(Document):
     def on_submit(self):
+        """
+        Validations on Submit
+        """
         validate_job_card_time_logs(self)
         validate_produced_qty_jc(self)
         update_job_card_status(self)
         self.validate_rm_qty_consumed()
         update_produced_qty(self)
-        update_planned_qty(self.production_item, frappe.get_value("Process Sheet", self.process_sheet,
-                                                                  "fg_warehouse"))
+        update_planned_qty(self.production_item, frappe.get_value("Process Sheet",
+            self.process_sheet, "fg_warehouse"))
         create_submit_ste_from_job_card(self)
         update_pro_sheet_rm_from_jc(self)
         self.update_next_jc_status()
         self.create_new_jc_if_needed()
 
     def on_update_after_submit(self):
+        """
+        Things done after submission of JCR and update
+        """
         update_job_card_qty_available(self)
         update_process_sheet_operations(ps_name=self.process_sheet, op_name=self.operation_id)
 
     def on_cancel(self):
+        """
+        Things done on Cancel of JCR
+        """
         update_job_card_status(self)
         cancel_delete_ste(self)
         update_produced_qty(self, status="Cancel")
-        update_planned_qty(self.production_item, frappe.get_value("Process Sheet", self.process_sheet,
-                                                                  "fg_warehouse"))
+        update_planned_qty(self.production_item, frappe.get_value("Process Sheet",
+            self.process_sheet, "fg_warehouse"))
         update_pro_sheet_rm_from_jc(self, status="Cancel")
         self.update_next_jc_status()
         update_process_sheet_operations(ps_name=self.process_sheet, op_name=self.operation_id)
 
     def validate(self):
+        """
+        Validation on Saving a JCR
+        """
         update_jc_posting_date_time(self)
         update_job_card_qty_available(self)
         update_job_card_priority(self)
@@ -112,54 +124,70 @@ class ProcessJobCardRIGPL(Document):
             update_jc_rm_status(self)
 
     def update_job_card_qty_text(self):
+        """
+        Updates the Quantity Text So that Items for Production is Not Needed to be Open
+        """
         it_doc = frappe.get_doc("Item", self.production_item)
-        qd = get_quantities_for_item(it_doc)
-        ready = qd.finished_qty + qd.dead_qty
-        qty_for_order = qd.on_so - ready - qd.on_po
-        min_needed = qd.reserved_for_prd + qd.on_so - qd.on_po - ready - qd.dead_qty + qd.calculated_rol
-        text = f"SO = {qd.on_so} \n PO = {qd.on_po} \n Needed for PRD = {qd.reserved_for_prd} \n " \
-               f"Finished Stock = {qd.finished_qty} \n Dead Stock = {qd.dead_qty} \n " \
-               f"Work In Progress = {qd.wip_qty} \n" \
-               f"Real ROL = {qd.re_order_level} But Calculated ROL = {qd.calculated_rol} \n " \
-               f"Minimum Qty to be Produced = {min_needed} AND Qty Needed for Order = {qty_for_order}"
-        if self.quantity_details != text:
-            self.quantity_details = text
+        qdict = get_quantities_for_item(it_doc)
+        ready = qdict.finished_qty + qdict.dead_qty
+        qty_for_order = qdict.on_so - ready - qdict.on_po
+        min_needed = (qdict.reserved_for_prd + qdict.on_so - qdict.on_po - ready -
+            qdict.dead_qty + qdict.calculated_rol)
+        qty_text = f"SO = {qdict.on_so} \nPO = {qdict.on_po} \nNeeded for PRD = "
+        f"{qdict.reserved_for_prd} \n Finished Stock = {qdict.finished_qty} \nDead Stock = "
+        f"{qdict.dead_qty} \n Work In Progress = {qdict.wip_qty} \nReal ROL = "
+        f"{qdict.re_order_level} But Calculated ROL = {qdict.calculated_rol} \nMinimum Qty to "
+        f"be Produced = {min_needed} AND Qty Needed for Order = {qty_for_order}"
+        if self.quantity_details != qty_text:
+            self.quantity_details = qty_text
 
     def update_calculated_qty(self, rm_item_dict):
+        """
+        Updates the Calculated Quantity in WIP Consumed and RM Consumed Tables of JCR
+        """
         wip_rm_consume = 0
         fg_rm_qty = self.total_completed_qty + self.total_rejected_qty
         if fg_rm_qty == 0:
             fg_rm_qty = self.for_quantity
         pro_sheet_doc = frappe.get_doc("Process Sheet", self.process_sheet)
-        for d in self.item_manufactured:
-            wip_calc_qty_dict = calculated_value_from_formula(rm_item_dict, d.item_code, fg_qty=d.qty,
-                                                              bom_template_name=pro_sheet_doc.bom_template,
-                                                              process_sheet_name=self.process_sheet, is_wip=1)
+        for wip in self.item_manufactured:
+            wip_calc_qty_dict = calculated_value_from_formula(rm_item_dict, wip.item_code,
+                fg_qty=wip.qty, bom_template_name=pro_sheet_doc.bom_template,
+                process_sheet_name=self.process_sheet, is_wip=1)
             for wip_it in wip_calc_qty_dict:
-                if d.item_code == wip_it.fg_item_code:
+                if wip.item_code == wip_it.fg_item_code:
                     wip_rm_consume += wip_it.qty
-                    if d.calculated_qty != wip_it.qty:
-                        d.calculated_qty = int(wip_it.qty)
+                    if wip.calculated_qty != wip_it.qty:
+                        wip.calculated_qty = int(wip_it.qty)
         rm_calc_qty_dict = calculated_value_from_formula(rm_item_dict, self.production_item,
-                                                         fg_qty=fg_rm_qty, bom_template_name=pro_sheet_doc.bom_template,
-                                                         process_sheet_name=self.process_sheet)
-        for rm in self.rm_consumed:
+            fg_qty=fg_rm_qty, bom_template_name=pro_sheet_doc.bom_template,
+            process_sheet_name=self.process_sheet)
+        for rmr in self.rm_consumed:
             for rm_it in rm_calc_qty_dict:
-                if rm.item_code == rm_it.rm_item_code:
+                if rmr.item_code == rm_it.rm_item_code:
                     wip_rm_consume += rm_it.qty
-                    rm.calculated_qty = int(wip_rm_consume)
+                    rmr.calculated_qty = int(wip_rm_consume)
 
     def get_items_from_process_sheet(self):
+        """
+        Gets Items from Process Sheet to Make JCR same as Process Sheet incase of RM Consumed and
+        WIP Manufactured
+        """
         get_items_from_process_sheet_for_job_card(self, "rm_consumed")
         get_items_from_process_sheet_for_job_card(self, "item_manufactured")
 
     def validate_rm_qty_consumed(self):
+        """
+        Validates the Raw Material Consumed Quantity can never be more than the Quantity
+        Available when Submitting a JCR. Also checks a function of check qty Job Card
+        """
         for row in self.rm_consumed:
             if flt(row.qty) > flt(row.qty_available):
-                frappe.throw('Available Qty = {} but Qty Needed = {} for Row# {} and Item Code: {} in '
-                             'Warehouse {}'.format(row.qty_available, row.qty, row.idx, row.item_code,
-                                                   row.source_warehouse), title="Warehouse Shortage")
-            check_qty_job_card(self, row, row.calculated_qty, row.qty, row.uom, row.bypass_qty_check)
+                frappe.throw(f"Available Qty = {row.qty_available} but Qty Needed = {row.qty} "
+                    f"for Row# {row.idx} and Item Code: {row.item_code} in Warehouse "
+                    f"{row.source_warehouse}", title="Warehouse Shortage")
+            check_qty_job_card(self, row, row.calculated_qty, row.qty, row.uom,
+                row.bypass_qty_check)
 
     def update_next_jc_status(self):
         nxt_jc_list = get_next_job_card(self.name)
