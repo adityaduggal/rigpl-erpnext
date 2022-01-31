ccf41a6962a52feef77ba8a67f503b3a66fddfc8
Aditya Duggal <adityaduggal@gmail.com>
2022-01-31 23:17:10 +0530

    fix: Removed the Validated email fields JS scripts

--- rigpl_erpnext/fixtures/custom_scripts/employee.js
+++ rigpl_erpnext/fixtures/custom_scripts/employee.js
@@ -17,16 +17,5 @@ frappe.ui.form.on("Employee", {
 				}
 			};
 		});
-	},
-    company_email: function(frm, cdt, cdn){
-        var d = locals[cdt][cdn]
-        frappe.model.set_value(cdt, cdn, "company_email_validated", 0);
-        cur_frm.refresh_fields();
-    },
-
-    personal_email: function(frm, cdt, cdn){
-        var d = locals[cdt][cdn]
-        frappe.model.set_value(cdt, cdn, "personal_email_validated", 0);
-        cur_frm.refresh_fields();
-    }
+	}
 });
