cd3269d32c72249e31f0f1fa1d19ee3660658e72
Aditya Duggal <adityaduggal@gmail.com>
2022-08-15 02:36:02 +0530

    feat: Now requirements is installed from setup

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
-from setuptools import setup, find_packages
-import os
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-version = '0.0.1'
+import ast
+import re
+
+from setuptools import find_packages, setup
+
+# get version from __version__ variable in rigpl_erpnext/__init__.py
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
+
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
+with open("rigpl_erpnext/__init__.py", "rb") as f:
+    version = str(ast.literal_eval(
+        _version_re.search(f.read().decode("utf-8")).group(1)))
 
 setup(
     name='rigpl_erpnext',
     version=version,
     description='Rohit ERPNext Extensions',
-    author='Rohit Industries Group Pvt. Ltd.',
+    author='Rohit Industries Group Pvt Ltd.',
     author_email='aditya@rigpl.com',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=("frappe",),
+    install_requires=install_requires,
 )
