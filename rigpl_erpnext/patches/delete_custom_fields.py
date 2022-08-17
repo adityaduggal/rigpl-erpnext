65c569b971dc3dc37c8f4a345f746203e5dde07f
Aditya Duggal <adityaduggal@gmail.com>
2022-08-17 17:00:50 +0530

    fix: Removed shift field from Attendance as its now standard field

---
+++ rigpl_erpnext/patches/delete_custom_fields.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import frappe
+
+
+def execute():
+    # Deletes custom fields from Custom Field Table based on Dictionary
+    custom_field_list = [{"dt": "Attendance", "fieldname": "shift"}
+                         ]
+    for fld in custom_field_list:
+        custom_field = frappe.db.get_value("Custom Field", fld)
+        if custom_field:
+            frappe.delete_doc("Custom Field", custom_field)
+            print(f"Deleted Custom Field: {custom_field} with Filters: {fld}")
