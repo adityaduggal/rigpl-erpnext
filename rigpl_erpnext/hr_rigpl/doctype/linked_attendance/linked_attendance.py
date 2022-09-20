3521ebe916dd2360e7e682967f0c8f3839484b50
Aditya Duggal <adityaduggal@gmail.com>
2022-09-20 11:40:03 +0530

    Added Linked Attendance table for Leave Allocation

---
+++ rigpl_erpnext/hr_rigpl/doctype/linked_attendance/linked_attendance.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Rohit Industries Group Private Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class LinkedAttendance(Document):
+	pass
