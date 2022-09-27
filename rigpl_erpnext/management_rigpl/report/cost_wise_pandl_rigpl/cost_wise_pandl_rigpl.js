bbc981f15920b7ac8c7d805b87e03e4f3473f2ed
Aditya Duggal <adityaduggal@gmail.com>
2022-09-27 09:55:13 +0530

    fix: Moved Cost wise Proft and Loss Statement to Management Module

---
+++ rigpl_erpnext/management_rigpl/report/cost_wise_pandl_rigpl/cost_wise_pandl_rigpl.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Rohit Industries Group Private Limited and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Cost Wise PandL RIGPL"] = {
+	"filters": [
+
+	]
+};
---
+++ rigpl_erpnext/management_rigpl/report/cost_wise_pandl_rigpl/cost_wise_pandl_rigpl.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Rohit Industries Group Private Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
