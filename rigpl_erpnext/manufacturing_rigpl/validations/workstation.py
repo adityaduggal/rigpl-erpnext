90496984d5ec20ca80d377a5f4cd029c354e977e
kalelkar-noman <kalelkarnoman014@gmail.com>
2025-11-24 16:26:03 +0000

    fix: undo not required changes

--- rigpl_erpnext/manufacturing_rigpl/validations/workstation.py
+++ rigpl_erpnext/manufacturing_rigpl/validations/workstation.py
@@ -10,17 +10,15 @@ def validate(doc, method):
     # Operation Table should have atleast one Operation
     allowed_ops = []
     if not doc.workstation_operation:
-        print(f"Atleast One Operation is Mandatory for the Workstation")
-        # frappe.throw(f"Atleast One Operation is Mandatory for the Workstation")
+        frappe.throw(f"Atleast One Operation is Mandatory for the Workstation")
     else:
         for d in doc.workstation_operation:
             if d.allowed_operation not in allowed_ops:
                 allowed_ops.append(d.allowed_operation)
             else:
-                print(f"For Row# {d.idx} and Operation: {d.allowed_operation} is Repeated")
-                # frappe.throw(
-                #     f"For Row# {d.idx} and Operation: {d.allowed_operation} is Repeated"
-                # )
+                frappe.throw(
+                    f"For Row# {d.idx} and Operation: {d.allowed_operation} is Repeated"
+                )
 
 
 @frappe.whitelist()
